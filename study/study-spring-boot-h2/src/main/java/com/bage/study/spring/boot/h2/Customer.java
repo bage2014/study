21fa9c12353b74cfa0622d2c0a80a6ac815a823c
bage2014 <893542907@qq.com>
2018-06-04 22:36:03 +0800

    增加高并发的学习入门

---
+++ study/study-spring-boot-h2/src/main/java/com/bage/study/spring/boot/h2/Customer.java
@@ -0,0 +1,46 @@
+package com.bage.study.spring.boot.h2;
+
+
+public class Customer {
+    private long id;
+    private String firstName, lastName;
+
+    public Customer(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+
+    // getters & setters omitted for brevity
+}
