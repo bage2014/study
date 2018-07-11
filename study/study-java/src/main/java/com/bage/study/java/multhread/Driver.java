d00efc7feadc04340edc0b10196a3eeef76a8fa0
bage2014 <893542907@qq.com>
2018-07-11 23:01:38 +0800

    增加对 CountDownLatch 的基本使用

---
+++ study/study-java/src/main/java/com/bage/study/java/multhread/Driver.java
@@ -0,0 +1,49 @@
+package com.bage.study.java.multhread;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Driver { // ...
+	void main(int N) throws InterruptedException {
+		CountDownLatch startSignal = new CountDownLatch(1);
+		CountDownLatch doneSignal = new CountDownLatch(N);
+
+		for (int i = 0; i < N; ++i) // create and start threads
+			new Thread(new Worker(startSignal, doneSignal,i)).start();
+
+		doSomethingElse(); // don't let run yet
+		startSignal.countDown(); // let all threads proceed
+		doSomethingElse();
+		doneSignal.await(); // wait for all to finish
+		doSomethingElse();
+	}
+
+	private void doSomethingElse() {
+		System.out.println("doSomethingElse is work");
+
+	}
+}
+
+class Worker implements Runnable {
+	private final CountDownLatch startSignal;
+	private final CountDownLatch doneSignal;
+	private final int i;
+
+	Worker(CountDownLatch startSignal, CountDownLatch doneSignal,int i) {
+		this.startSignal = startSignal;
+		this.doneSignal = doneSignal;
+		this.i = i;
+	}
+
+	public void run() {
+		try {
+			startSignal.await();
+			doWork();
+			doneSignal.countDown();
+		} catch (InterruptedException ex) {
+		} // return;
+	}
+
+	void doWork() {
+		System.out.println("doWork is work:" + i);
+	}
+}
---
+++ study/study-java/src/main/java/com/bage/study/java/multhread/MyCountDownLatch.java
@@ -0,0 +1,67 @@
+package com.bage.study.java.multhread;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * CountDownLatch 的使用
+ * @author bage
+ *
+ */
+public class MyCountDownLatch {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		// defaultExample();
+		long before = System.currentTimeMillis();
+		int n = 400;
+		CountDownLatch countDownLatch = new CountDownLatch(n);
+		CountDownLatch startLatch = null;//new CountDownLatch(1);
+
+		for (int i = 0; i < n; i++) {
+			new thread11(countDownLatch,i,startLatch){}.start();
+		}
+		
+		System.out.println("所有线程执行开始:" + (System.currentTimeMillis() - before));
+		before = System.currentTimeMillis();
+		startLatch.countDown();
+		countDownLatch.await();
+		System.out.println("所有线程执行结束：" + (System.currentTimeMillis() - before));
+
+	}
+
+	private static void defaultExample() {
+		try {
+			new Driver().main(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	
+}
+
+class thread11 extends Thread {
+	private CountDownLatch countDownLatch;
+	private CountDownLatch startLatch;
+	private int i;
+
+	public thread11(CountDownLatch countDownLatch, int i, CountDownLatch startLatch){
+		this.countDownLatch = countDownLatch;
+		this.i= i;
+		this.startLatch = startLatch;
+	}
+	
+	public void run() {
+		try {
+			if(startLatch != null){
+				startLatch.await();
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("线程"+i+"启动...");
+		countDownLatch.countDown();
+	}
+};
