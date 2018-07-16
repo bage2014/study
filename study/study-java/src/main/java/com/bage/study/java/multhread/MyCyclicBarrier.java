549e778aa38c5a7ac26c65c899cf5cbaf7b11e95
bage2014 <893542907@qq.com>
2018-07-16 13:01:38 +0800

    CyclicBarrier 最基本用法demo

--- study/study-java/src/main/java/com/bage/study/java/multhread/MyCyclicBarrier.java
+++ study/study-java/src/main/java/com/bage/study/java/multhread/MyCyclicBarrier.java
@@ -1,85 +1,70 @@
 package com.bage.study.java.multhread;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 
+/**
+ *  CyclicBarrier 最基本用法
+ * @author bage
+ *
+ */
 public class MyCyclicBarrier {
 
 	public static void main(String[] args) {
 
-		CyclicBarrier cyclicBarrier = new CyclicBarrier(100);
-
-		//new Solver.Worker().run();
+		int n = 10;
+		CyclicBarrier cyclicBarrier = new CyclicBarrier(n);
+		CountDownLatch countDownLatch = new CountDownLatch(n);
+		
+		for (int i = 0; i < n; i++) {
+			new ThreadC(i,cyclicBarrier,countDownLatch).start();
+		}
+		
+		try {
+			countDownLatch.await();
+			System.out.println("所有程序执行结束。。。");
+			//cyclicBarrier.reset();
+			
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
 
-class Solver {
-	final int N;
-	final float[][] data;
-	final CyclicBarrier barrier;
-
-	class Worker implements Runnable {
-		int myRow;
-
-		Worker(int row) {
-			myRow = row;
-		}
-
-		public void run() {
-			while (!done()) {
-				processRow(myRow);
-
-				try {
-					barrier.await();
-				} catch (InterruptedException ex) {
-					return;
-				} catch (BrokenBarrierException ex) {
-					return;
-				}
-			}
-		}
-
-		private void processRow(int myRow) {
-			System.out.println("processRow:" + myRow);
-		}
-
-		private boolean done() {
-			System.out.println("done");
-			return false;
-		}
+class ThreadC extends Thread{
+	
+	final int i;
+	final CyclicBarrier cyclicBarrier;
+	final CountDownLatch countDownLatch;
+	
+	public ThreadC(int i, CyclicBarrier cyclicBarrier, CountDownLatch countDownLatch){
+		this.i = i;
+		this.cyclicBarrier = cyclicBarrier;
+		this.countDownLatch = countDownLatch;
 	}
+	
+	@Override
+	public void run() {
+		try {
+			System.out.println("run:" + i + "开始准备");
+			int t = 1000 * new Random().nextInt(10) ;
+			Thread.sleep(t);
+			System.out.println("run:" + i + "准备就绪，(t="+t+")");
 
-	public Solver(float[][] matrix) {
-		data = matrix;
-		N = matrix.length;
-		Runnable barrierAction = new Runnable() {
-			public void run() {
-				mergeRows();
-			}
-
-			private void mergeRows() {
-				System.out.println("mergeRows:");
-			}
-		};
-		barrier = new CyclicBarrier(N, barrierAction);
-
-		List<Thread> threads = new ArrayList<Thread>(N);
-		for (int i = 0; i < N; i++) {
-			Thread thread = new Thread(new Worker(i));
-			threads.add(thread);
-			thread.start();
+			cyclicBarrier.await();
+			System.out.println("run:" + i  + "执行结束");
+			countDownLatch.countDown();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BrokenBarrierException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-
-		// wait until done
-		for (Thread thread : threads)
-			try {
-				thread.join();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 	}
 }
