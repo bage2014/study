18d41a08b3ff279e3b99acc32e169f8348ea5aad
bage2014 <893542907@qq.com>
2018-08-12 23:26:47 +0800

    将主题抽取接口

--- study/study-java/src/main/java/com/bage/study/java/pattern/observer/Main.java
+++ study/study-java/src/main/java/com/bage/study/java/pattern/observer/Main.java
@@ -1,7 +1,10 @@
 package com.bage.study.java.pattern.observer;
 
 /**
- * 观察者模式
+ * 观察者模式<br>
+ * <br>
+ * 在对象之间定义了一对多的依赖，这样一来，当一个对象改变状态，依赖它的对象会收到通知并自动更新。<br>
+ * 
  * @author bage
  *
  */
@@ -9,9 +12,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Subject subject = new Subject();
-		Observer observer1 = new Observer1(subject);
-		Observer observer2 = new Observer2(subject);
+		Subject subject = new ConcreteSubject(); // 被观察者
+		
+		Observer observer1 = new Observer1(subject); // 观察者1
+		Observer observer2 = new Observer2(subject); // 观察者2
 		subject.bindObservers(observer1  );
 		subject.bindObservers(observer2  );
 
@@ -22,6 +26,7 @@ public class Main {
 		observer1.getSubject().unbindObservers(observer1);
 		
 		subject.updateData("newData2");
+		
 		
 	}
 	
--- study/study-java/src/main/java/com/bage/study/java/pattern/observer/Subject.java
+++ study/study-java/src/main/java/com/bage/study/java/pattern/observer/Subject.java
@@ -1,39 +1,17 @@
 package com.bage.study.java.pattern.observer;
 
-import java.util.LinkedList;
 import java.util.List;
 
-/**
- * 
- * @author bage
- *
- */
-public class Subject {
+public interface Subject {
 
-	private List<Observer> observers = new LinkedList<>();
-	
-	public void bindObservers(Observer observer) {
-		observers.add(observer);
-	}
-	
-	public void unbindObservers(Observer observer) {
-		if(observers.contains(observer)){
-			observers.remove(observer);
-		}
-	}
-	
-	public List<Observer> getObservers() {
-		return observers;
-	}
+	void bindObservers(Observer observer);
 
-	public void setObservers(List<Observer> observers) {
-		this.observers = observers;
-	}
-	
-	public void updateData(Object newData){
-		for (int i = 0; i < observers.size(); i++) {
-			observers.get(i).change(newData);
-		}
-	}
-	
+	void unbindObservers(Observer observer);
+
+	List<Observer> getObservers();
+
+	void setObservers(List<Observer> observers);
+
+	void updateData(Object newData);
+
 }
