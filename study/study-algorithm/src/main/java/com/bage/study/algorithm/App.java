e5dfc86626cb639cd1ac7683467dc6ca37772399
bage2014 <893542907@qq.com>
2018-08-23 23:44:56 +0800

    增加对数据库事务的理解，原子性一致性隔离性持久性，以及隔离等级

---
+++ study/study-algorithm/src/main/java/com/bage/study/algorithm/App.java
@@ -0,0 +1,14 @@
+package com.bage.study.algorithm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println(Math.sqrt(4));
+        System.out.println( "Hello World!" );
+    }
+}
