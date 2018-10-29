eab8614f878018eb9c5b45ec3407deb1dad792e8
luruihua <luruihua@luruihua.luculent.net>
2018-10-29 17:41:21 +0800

    迁移之前的spring oauth2的代码[未完成]

---
+++ study/study-spring-boot-oauth2-client1/src/main/java/com/bage/study/spring/boot/oauth2/client1/SocialApplication.java
@@ -0,0 +1,42 @@
+package com.bage.study.spring.boot.oauth2.client1;
+
+import java.security.Principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+@RestController
+public class SocialApplication extends WebSecurityConfigurerAdapter {
+
+  @RequestMapping("/user")
+  public Principal user(Principal principal) {
+    return principal;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(SocialApplication.class, args);
+  }
+  
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+      .antMatcher("/**")
+      .authorizeRequests()
+        .antMatchers("/", "/login**", "/webjars/**", "/error**")
+        .permitAll()
+      .anyRequest()
+        .authenticated()
+        .and().logout().logoutSuccessUrl("/").permitAll()
+        .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+        ;
+  }
+
+}
---
+++ study/study-spring-boot-oauth2-server/src/main/java/com/bage/study/spring/boot/oauth2/server/App.java
@@ -0,0 +1,13 @@
+package com.bage.study.spring.boot.oauth2.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
