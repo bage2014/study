8772623797d5a2fce812db147c2984eec2a95d65
bage2014 <893542907@qq.com>
2019-05-12 23:40:14 +0800

    集成 lombok 和基本使用

--- study-lombok/src/main/java/com/bage/study/lombok/App.java
+++ study-lombok/src/main/java/com/bage/study/lombok/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println(new User("id","name").getId());
         System.out.println( "Hello World!" );
     }
 }
--- study-lombok/src/main/java/com/bage/study/lombok/User.java
+++ study-lombok/src/main/java/com/bage/study/lombok/User.java
@@ -1,18 +1,17 @@
 package com.bage.study.lombok;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
 	private String id;
 	private String name;
 	
-	public static void main(String[] args) {
-	}
+
 	
 }
