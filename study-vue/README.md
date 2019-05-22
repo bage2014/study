b840d3efabad87b6abe08ff77c778db8068ba462
bage2014 <893542907@qq.com>
2019-05-23 00:07:53 +0800

    vue 入门使用

--- study-vue/README.md
+++ study-vue/README.md
@@ -18,3 +18,62 @@ Vue 学习笔记
 
 ### 部署运行 ###
     npm run dev
+
+
+## 学习笔记 ##
+
+### 起步 ###
+
+#### 声明式渲染 ####
+
+{{ message }}模板渲染
+
+    <div id="app">
+      {{ message }}
+    </div>
+
+v-bind 指令
+
+    <div id="app-2">
+      <span v-bind:title="message">
+    鼠标悬停几秒钟查看此处动态绑定的提示信息！
+      </span>
+    </div>
+
+#### 条件与循环 ####
+
+v-if 控制切换
+
+    <div id="app-3">
+      <p v-if="seen">现在你看到我了</p>
+    </div>
+
+-for 循环
+
+<div id="app-4">
+  <ol>
+<li v-for="todo in todos">
+  {{ todo.text }}
+</li>
+  </ol>
+</div>
+
+#### 处理用户输入 ####
+
+v-on 指令
+
+    <div id="app-5">
+      <p>{{ message }}</p>
+      <button v-on:click="reverseMessage">逆转消息</button>
+    </div>
+
+v-model 指令
+
+    <div id="app-6">
+      <p>{{ message }}</p>
+      <input v-model="message">
+    </div>
+
+
+#### 组件化应用构建 ####
+// TODO
---
+++ study-vue/src/main/resources/static/index.js
@@ -0,0 +1,57 @@
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: '页面加载于 ' + new Date().toLocaleString()
+    }
+})
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+})
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: '学习 JavaScript' },
+            { text: '学习 Vue' },
+            { text: '整个牛项目' }
+        ]
+    }
+})
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
+
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+// 定义名为 todo-item 的新组件
+Vue.component('todo-item', {
+    template: '<li>这是个待办项</li>'
+})
