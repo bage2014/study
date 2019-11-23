1130e708878c4aace523eafd28e0206d45dc6486
bage2014 <893542907@qq.com>
2019-11-24 00:19:18 +0800

    web filter

--- study-spring-boot/src/main/java/hello/Application.java
+++ study-spring-boot/src/main/java/hello/Application.java
@@ -2,8 +2,10 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan(basePackages = {"hello"})
 public class Application {
 
     public static void main(String[] args) {
