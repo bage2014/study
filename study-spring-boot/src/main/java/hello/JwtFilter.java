af58aa60c59c0e06d152bb91df6c6889c43f1383
bage2014 <893542907@qq.com>
2019-11-24 00:20:06 +0800

    web filter

---
+++ study-spring-boot/src/main/java/hello/JwtFilter.java
@@ -0,0 +1,37 @@
+package hello;
+
+import org.springframework.core.annotation.Order;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+import java.util.Objects;
+
+@WebFilter(urlPatterns = "/*")
+@Order(1)
+public class JwtFilter implements Filter {
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("AuthFilter request = " + servletRequest);
+
+        String jwt = servletRequest.getParameter("jwt");
+        System.out.println("param jwt = " + jwt);
+        if(Objects.isNull(jwt)){
+            servletResponse.getWriter().append("not login ...");
+            System.out.println("jwt = " + jwt);
+            return ;
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
