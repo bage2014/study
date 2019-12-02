902d78813110a0a5d2959fe6ed8920a0b4fd8d05
bage2014 <893542907@qq.com>
2019-12-02 22:56:11 +0800

    shared-jdbc example

---
+++ study-split-db-tb/src/main/java/com/bage/App.java
@@ -0,0 +1,75 @@
+package com.bage;
+
+import org.apache.shardingsphere.api.config.sharding.ShardingRuleConfiguration;
+import org.apache.shardingsphere.api.config.sharding.TableRuleConfiguration;
+import org.apache.shardingsphere.api.config.sharding.strategy.InlineShardingStrategyConfiguration;
+import org.apache.shardingsphere.shardingjdbc.api.ShardingDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) throws SQLException {
+
+        // 配置真实数据源
+        Map<String, DataSource> dataSourceMap = new HashMap<>();
+
+        // 配置第一个数据源
+        BasicDataSource dataSource1 = new BasicDataSource();
+        dataSource1.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource1.setUrl("jdbc:mysql://localhost:3306/ds0");
+        dataSource1.setUsername("root");
+        dataSource1.setPassword("");
+        dataSourceMap.put("ds0", dataSource1);
+
+        // 配置第二个数据源
+        BasicDataSource dataSource2 = new BasicDataSource();
+        dataSource2.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource2.setUrl("jdbc:mysql://localhost:3306/ds1");
+        dataSource2.setUsername("root");
+        dataSource2.setPassword("");
+        dataSourceMap.put("ds1", dataSource2);
+
+        // 配置Order表规则
+        TableRuleConfiguration orderTableRuleConfig = new TableRuleConfiguration("t_order", "ds${0..1}.t_order${0..1}");
+
+        // 配置分库 + 分表策略
+        orderTableRuleConfig.setDatabaseShardingStrategyConfig(new InlineShardingStrategyConfiguration("user_id", "ds${user_id % 2}"));
+        orderTableRuleConfig.setTableShardingStrategyConfig(new InlineShardingStrategyConfiguration("order_id", "t_order${order_id % 2}"));
+
+        // 配置分片规则
+        ShardingRuleConfiguration shardingRuleConfig = new ShardingRuleConfiguration();
+        shardingRuleConfig.getTableRuleConfigs().add(orderTableRuleConfig);
+
+        // 省略配置order_item表规则...
+
+        // 获取数据源对象
+        DataSource dataSource = ShardingDataSourceFactory.createDataSource(dataSourceMap, shardingRuleConfig, new Properties());
+
+
+        String sql = "SELECT i.* FROM t_order o JOIN t_order_item i ON o.order_id=i.order_id WHERE o.user_id=? AND o.order_id=?";
+        try (
+                Connection conn = dataSource.getConnection();
+                PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+            preparedStatement.setInt(1, 10);
+            preparedStatement.setInt(2, 1001);
+            try (ResultSet rs = preparedStatement.executeQuery()) {
+                while (rs.next()) {
+                    System.out.println(rs.getInt(1));
+                    System.out.println(rs.getInt(2));
+                }
+            }
+        }
+
+        System.out.println("Hello World!");
+    }
+}
