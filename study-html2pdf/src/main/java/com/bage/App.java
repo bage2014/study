6f882c52c73385be0870963d9d7b0d78f969d756
bage2014 <893542907@qq.com>
2020-01-08 22:51:19 +0800

    from url

--- study-html2pdf/src/main/java/com/bage/App.java
+++ study-html2pdf/src/main/java/com/bage/App.java
@@ -1,13 +1,20 @@
 package com.bage;
 
+import io.woo.htmltopdf.HtmlToPdf;
+import io.woo.htmltopdf.HtmlToPdfObject;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
+//        boolean success = HtmlToPdf.create()
+//                .object(HtmlToPdfObject.forHtml("<p><em>Apples</em>, not oranges</p>"))
+//                .convert("C:\\Users\\bage\\Desktop\\file.pdf");
+
         boolean success = HtmlToPdf.create()
-                .object(HtmlToPdfObject.forHtml("<p><em>Apples</em>, not oranges</p>"))
-                .convert("/path/to/file.pdf");
-        System.out.println("Hello World!");
+                .object(HtmlToPdfObject.forUrl("https://github.com/wooio/htmltopdf-java"))
+                .convert("C:\\Users\\bage\\Desktop\\file.pdf");
+
     }
 }
