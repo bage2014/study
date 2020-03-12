1700b2155db1fc0373cfa91385d748cffb38f5ac
bage2014 <893542907@qq.com>
2020-03-12 22:18:08 +0800

    completeFuture

---
+++ study-java/src/main/java/com/bage/study/java/multhread/CompleteFutureTest.java
@@ -0,0 +1,71 @@
+package com.bage.study.java.multhread;
+
+import java.util.concurrent.CompletableFuture;
+
+public class CompleteFutureTest {
+
+    public static void main(String[] args) {
+
+        // supplyAsync 基本用法，异步响应,有响应结果
+        CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(2000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "ABC";
+        }).whenComplete((item,e) -> {
+            System.out.println(item);
+        });
+
+        // runAsync 无返回值
+        CompletableFuture.runAsync(() -> {
+            try {
+                Thread.sleep(2000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            String abc = "ABCD";
+            System.out.println(abc);
+        }).whenComplete((item,e) -> {
+            System.out.println(item);
+        });
+
+        // supplyAsync 基本用法，异步响应,有响应结果
+        CompletableFuture<String> cf1 = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(3000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "3000L";
+        }).whenComplete((item, e) -> {
+            System.out.println(item);
+        });
+        CompletableFuture<String> cf2 = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(4000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "4000L";
+        }).whenComplete((item, e) -> {
+            System.out.println(item);
+        });
+        // 所有结束，才结束，无返回值
+        CompletableFuture<Void> voidCompletableFuture = CompletableFuture.allOf(cf1, cf2).whenComplete((item, e) -> {
+            System.out.println("allOf:" + item);
+        });
+        // 任意结束，即结束
+        CompletableFuture<Object> objectCompletableFuture = CompletableFuture.anyOf(cf1, cf2).whenComplete((item, e) -> {
+            System.out.println("anyOf:" + item);
+        });
+        // 等待结束
+        try {
+            Thread.sleep(6000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
