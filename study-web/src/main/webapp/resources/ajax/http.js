a7d7617e69b9203e1111a0ae910476856adbc529
bage2014 <893542907@qq.com>
2020-07-04 21:15:24 +0800

    ajax utils testcase

--- study-web/src/main/webapp/resources/ajax/http.js
+++ study-web/src/main/webapp/resources/ajax/http.js
@@ -2,26 +2,44 @@
 var Http = function(){
     return {
         baseUrl: '',
-        get: function(url,data,callBack) {
-            return this.request(url,data,callback,‘POST’);
+        jsonGet: function(url,json,callback,headers) {
+            this.request(url,json,callback,headers,'GET');
         },
-        post: function(url,data,callBack) {
-            return this.request(url,data,callback,‘GET’);
+        jsonPost: function(url,json,callback,headers) {
+            this.request(url,json,callback,headers,'POST');
         },
-        request: function(url,data,callback,method) {
+        get: function(url,paramPairs,callback,headers) {
+            this.request(url,paramPairs,callback,headers,'GET');
+        },
+        post: function(url,paramPairs,callback,headers) {
+            this.request(url,paramPairs,callback,headers,'POST');
+        },
+        request: function(url,paramPairs,callback,headers,method) {
+        	if(false){
+        		this.jqueryAjax(url,paramPairs,callback,headers,method);
+        	} else {
+        		// 原生ajax 
+        		this.ajax(url,paramPairs,callback,headers,method);
+        	}
+            
+        },
+        ajax: function(url,paramPairs,callback,headers,method) {
+        	
+        	var datas = this.pairsToString(paramPairs);
+
             var xhr = null;
             if (window.XMLHttpRequest) {
                 xhr = new XMLHttpRequest
             } else {
                 xhr = new ActiveXObject('Microsoft.XMLHttp')
             }
-            if (method == 'GET') {
-                xhr.open(method, url+'?'+data, true);
+            if (method == 'POST') {
+            	xhr.open(method, url, true);
+                xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+                xhr.send(datas);
+            } else { // GET
+                xhr.open(method, url + datas, true);
                 xhr.send();
-            } else if (method == 'POST') {
-                xhr.open(method, url, true);
-                xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded')
-                xhr.send(data);
             }
 
             xhr.onreadystatechange = function () {
@@ -31,7 +49,33 @@ var Http = function(){
                     }
                 }
             }
-        }
+        },
+        
+        jqueryAjax: function(url,paramPairs,callback,headers,method) {
+            
+        },
+
+        pairsToString: function(paramPairs) {
+        	var params = "";
+            if(paramPairs){
+            	for(var index in paramPairs){
+            		var pair = paramPairs[index];
+            		if(params == ""){
+            			params = params + "?";
+            		} else {
+            			params = params + "&";
+            		}
+            		// 获取第一个属性
+            		for(var key in pair){
+            		    params += key + "=" + pair[key];
+            			break;
+            		}
+            	}
+            }
+            return params;
+        },
+
+        
     };
 }();
 
