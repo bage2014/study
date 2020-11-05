e546c8307ce1444974c1077c3debaaa14ab621db
407chendaxia <chendaxia@aliyun.com>
2020-11-05 23:51:02 +0800

    添加两个简单的字符串算法

--- study-algorithm/src/main/java/com/bage/study/algorithm/string/Solution.java
+++ study-algorithm/src/main/java/com/bage/study/algorithm/string/Solution.java
@@ -130,4 +130,143 @@ public class Solution {
 //
 //        return min;
     }
+
+
+    /**
+     * 字符串压缩。利用字符重复出现的次数，编写一种方法，实现基本的字符串压缩功能。
+     * 比如，字符串aabcccccaaa会变为a2b1c5a3。若“压缩”后的字符串没有变短，则返回原先的字符串。
+     * 你可以假设字符串中只包含大小写英文字母（a至z）。
+     * @param S
+     * @return
+     */
+    public String compressString(String S) {
+
+        //非常简单，一目了然
+
+        int length = S.length();
+
+        if(length>1){
+            StringBuilder builder = new StringBuilder();
+
+            char c = S.charAt(0);
+            builder.append(c);
+            int n = 1;
+            for(int i=1; i<length; ++i){
+                char c1 = S.charAt(i);
+                if(c == c1){
+                    n++;
+                }else{
+                    builder.append(n);
+                    builder.append(c1);
+                    n=1;
+                    c = c1;
+                }
+            }
+            builder.append(n);
+
+            String s1 = builder.toString();
+            if(s1.length()>=S.length()){
+                return S;
+            }else{
+                return s1;
+            }
+
+        }
+
+        return S;
+    }
+
+    /**
+     * 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+     *
+     * 说明：本题中，我们将空字符串定义为有效的回文串。
+     * @param s
+     * @return
+     */
+    public boolean isPalindrome(String s) {
+
+        //非常基本的算法，从两头遍历即可
+
+        int length = s.length();
+
+        if(length>1){
+            int l = 0, r = length-1;
+
+            while (true){
+
+                //左边找到第一个是数字或者字母的下标
+                while (l<length){
+
+                    char c = s.charAt(l);
+
+                    if(isNum(c) || isAlphabet(c)){
+                        break;
+                    }
+                    l++;
+                }
+
+                //超出限制没找到，直接返回true
+                if(l>=length){
+                    return true;
+                }
+
+                //右边找到第一个是数字或者字母的下标
+                while (r>-1){
+                    char c = s.charAt(r);
+
+                    if(isNum(c) || isAlphabet(c)){
+                        break;
+                    }
+                    r--;
+                }
+
+                if(l>=r){
+                    //如果已经完成遍历，直接返回true
+                    return true;
+                }else{
+                    //判断两值是否相等
+                    char c1 = s.charAt(l);
+                    char c2 = s.charAt(r);
+                    if(c1 != c2){
+                        if(isAlphabet(c1) && isAlphabet(c2)){
+                            if(calNum(c1) != calNum(c2)){
+                                return false;
+                            }
+                        }else{
+                            return false;
+                        }
+                    }
+                }
+
+                //一次循环后别忘记把两个下标前移
+                l++;
+                r--;
+
+            }
+        }
+
+
+
+
+        return true;
+
+    }
+
+
+    private boolean isNum(char c){
+        return c>='0' && c<='9';
+    }
+
+    private boolean isAlphabet(char c){
+        return (c>='a' && c<='z') || (c>='A' && c<='Z');
+    }
+
+    private int calNum(char c){
+        if(c>='a' && c<='z'){
+            return c-'a';
+        }else{
+            return c-'A';
+        }
+    }
+
 }
