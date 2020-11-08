9c90eb2c44980f31ba4d62c77694cae7d0d217af
407chendaxia <chendaxia@aliyun.com>
2020-11-08 17:42:59 +0800

    分层遍历二叉树的算法

--- study-algorithm/src/main/java/com/bage/study/algorithm/tree/Solution.java
+++ study-algorithm/src/main/java/com/bage/study/algorithm/tree/Solution.java
@@ -1,5 +1,9 @@
 package com.bage.study.algorithm.tree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Solution {
     public static void main(String[] args){
 
@@ -66,6 +70,72 @@ public class Solution {
 
         return s.val == t.val && treeEquals(s.left, t.left) && treeEquals(s.right, t.right);
 
+    }
+
+
+    /**
+     * 分层遍历二叉树
+     * @param root
+     */
+    public void printNodeByLevel(TreeNode root){
+        //方法一 从根节点开始，打印二叉树的每一层，缺点：每次都是从根节点开始遍历
+        for(int i=0;;++i){
+            if(printNodeAtLevel(root,i) == 0){
+                break;
+            }
+            System.out.println();
+        }
+
+        //方法二 广度优先的算法
+        List<TreeNode> list = new ArrayList<TreeNode>();  //用于按层存放每个节点
+        list.add(root);
+        int cur = 0, last;    //cur代表已经遍历的节点，last代表当前层的最后一个节点
+
+        while (cur < list.size()){
+            last = list.size();  //每次都要重新初始化last
+
+            //打印某一层
+            while (cur < last){
+
+                TreeNode node = list.get(cur);
+
+                System.out.print(node.val);
+                System.out.print(' ');
+
+                if(node.left!=null){
+                    list.add(node.left);
+                }
+
+                if(node.right!=null){
+                    list.add(node.right);
+                }
+
+                cur++;
+
+            }
+
+            System.out.println();
+        }
+    }
+
+    /**
+     * 打印二叉树的某一层
+     * @param root
+     * @param level
+     */
+    private int printNodeAtLevel(TreeNode root, int level){
+        if(root == null){
+            return 0;
+        }
+
+        if(level == 0){
+            System.out.print(root.val);
+            System.out.print(' ');
+            return 1;
+        }else{
+            //打印二叉树的第n层，相当于打印两个孩子节点的第n-1层，这样就可以递归
+            return printNodeAtLevel(root.left, level-1)+printNodeAtLevel(root.right, level-1);
+        }
     }
 
 }
