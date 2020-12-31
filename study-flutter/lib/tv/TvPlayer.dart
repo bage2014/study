fd2a444c42cc6f5073039f98b4cdafc95c0458b2
bage2014 <893542907@qq.com>
2020-12-31 16:49:17 +0800

    fijkplayer

--- study-flutter/lib/tv/TvPlayer.dart
+++ study-flutter/lib/tv/TvPlayer.dart
@@ -1,37 +1,41 @@
-//
-//import 'package:fijkplayer/fijkplayer.dart';
-//
-//class _MyHomePageState extends State<MyHomePage> {
-//  final FijkPlayer player = FijkPlayer();
-//
-//  @override
-//  void initState() {
-//    super.initState();
-//    player.setDataSource(
-//        "https://sample-videos.com/video123/flv/240/big_buck_bunny_240p_10mb.flv",
-//        autoPlay: true);
-//  }
-//
-//  @override
-//  void dispose() {
-//    super.dispose();
-//    player.release();
-//  }
-//
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      appBar: AppBar(
-//        // Here we take the value from the MyHomePage object that was created by
-//        // the App.build method, and use it to set our appbar title.
-//        title: Text(widget.title),
-//      ),
-//      body: Center(
-//        // Center is a layout widget. It takes a single child and positions it
-//        // in the middle of the parent.
-//        child: FijkView(player: player),
-//      ),
-//    );
-//  }
-//}
+import 'package:fijkplayer/fijkplayer.dart';
+import 'package:flutter/material.dart';
+
+class VideoScreen extends StatefulWidget {
+  final String url;
+
+  VideoScreen({@required this.url});
+
+  @override
+  _VideoScreenState createState() => _VideoScreenState();
+}
+
+class _VideoScreenState extends State<VideoScreen> {
+  final FijkPlayer player = FijkPlayer();
+
+  _VideoScreenState();
+
+  @override
+  void initState() {
+    super.initState();
+    player.setDataSource(widget.url, autoPlay: true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Fijkplayer Example")),
+        body: Container(
+          alignment: Alignment.center,
+          child: FijkView(
+            player: player,
+          ),
+        ));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    player.release();
+  }
+}
