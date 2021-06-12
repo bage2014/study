6c186027ade39e8f0054a44bc438ec86ce238ea2
bage2014 <893542907@qq.com>
2021-06-12 22:03:24 +0800

    time line

--- study-flutter/lib/view/about/AboutAuthor.dart
+++ study-flutter/lib/view/about/AboutAuthor.dart
@@ -1,7 +1,8 @@
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/material.dart';
 import 'package:app_lu_lu/component/log/Logs.dart';
 import 'package:app_lu_lu/locale/Translations.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:timelines/timelines.dart';
 
 class AboutAuthor extends StatefulWidget {
   @override
@@ -13,6 +14,8 @@ class _AboutAuthor extends State<AboutAuthor> {
 
   @override
   Widget build(BuildContext context) {
+    const double edgeLeft = 16.0;
+    const double edgeRight = 16.0;
     return Scaffold(
       appBar: AppBar(
         title: Text(Translations.textOf(context, "about.author.title")),
@@ -21,33 +24,31 @@ class _AboutAuthor extends State<AboutAuthor> {
         alignment: Alignment.center,
         child: Column(children: <Widget>[
           Container(
-            padding: const EdgeInsets.fromLTRB(24.0, 8.0, 0.0, 0.0),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child:
-                  CachedNetworkImage(
-                    imageUrl: "https://avatars.githubusercontent.com/u/18094768?v=4",
-                    placeholder: (context, url) => CircularProgressIndicator(),
-                    errorWidget: (context, url, error) => Icon(Icons.error),
-                    width: 64, //图片的宽
-                    height: 64,
-                  ),
-                ),
-                Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center, //垂直方向居中对齐
-                    children: <Widget>[
-                      Text('陆瑞华', textScaleFactor: 1.2),
-                      Text("893542907@qq.com")
-                    ],
-                  ),
-                ),
-              ],
-            ),
+            padding: const EdgeInsets.fromLTRB(edgeLeft, 16.0, edgeRight, 0.0),
+            child: ClipOval(
+              child: CachedNetworkImage(
+                imageUrl:
+                    "https://avatars.githubusercontent.com/u/18094768?v=4",
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) =>
+                    Image(image: AssetImage("assets/images/user_null.png")),
+                height: 128,
+                width: 128,
+              ),
+            ), //            child: Text(Translations.textOf(context, "about.author.name")),
           ),
           Container(
-            padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
+            padding: const EdgeInsets.fromLTRB(edgeLeft, 0.0, edgeRight, 0.0),
+            child: Text('陆瑞华', textScaleFactor: 1.4),
+//            child: Text(Translations.textOf(context, "about.author.name")),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(edgeLeft, 0.0, edgeRight, 0.0),
+            child: Text('893542907@qq.com'),
+//            child: Text(Translations.textOf(context, "about.author.name")),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(edgeLeft, 16.0, edgeRight, 16.0),
             child: Text('上海某互联网，Java 研发工程师，5年研发服务端经验'),
 //            child: Text(Translations.textOf(context, "about.author.name")),
           ),
@@ -55,33 +56,41 @@ class _AboutAuthor extends State<AboutAuthor> {
             alignment: Alignment.centerLeft,
             child: RefreshIndicator(
               onRefresh: _onRefresh,
-              child: list.length == 0
-                  ? Center(
-                      child: Text(
-                        Translations.textOf(context, "all.list.view.no.data"),
-                        textAlign: TextAlign.center,
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    final data = _data(index + 1);
+                    return Center(
+                      child: Container(
+                        width: 360.0,
+                        child: Card(
+                          margin: EdgeInsets.all(20.0),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: _OrderTitle(
+                                  orderInfo: data,
+                                ),
+                              ),
+                              Divider(height: 1.0),
+                              _DeliveryProcesses(
+                                  processes: data.deliveryProcesses),
+                              Divider(height: 1.0),
+                              Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: _OnTimeBar(driver: data.driverInfo),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                    )
-                  : ListView.separated(
-                      itemCount: list.length,
-                      itemBuilder: (context, index) {
-                        return new GestureDetector(
-                            onTap: () {
-                              Logs.info('onTap index = $index');
-                            },
-                            child: ListTile(
-                                title: Text(list[index]),
-                                trailing: new GestureDetector(
-                                    child: Icon(
-                                  Icons.favorite,
-                                  color: Colors.red,
-                                ))));
-                      },
-                      separatorBuilder: (context, index) => Divider(height: .0),
-                    ),
+                    );
+                  },
+                ),
+              ),
             ),
-//            child: Text(Translations.textOf(context, "about.author.name")),
-          ),
         ]),
       ),
     );
@@ -108,5 +117,288 @@ class _AboutAuthor extends State<AboutAuthor> {
 //    }).catchError((error) {
 //      print(error.toString());
 //    });
+  }
+}
+
+class _OrderTitle extends StatelessWidget {
+  const _OrderTitle({
+    Key? key,
+    required this.orderInfo,
+  }) : super(key: key);
+
+  final _OrderInfo orderInfo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          'Delivery #${orderInfo.id}',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Spacer(),
+        Text(
+          '${orderInfo.date.day}/${orderInfo.date.month}/${orderInfo.date.year}',
+          style: TextStyle(
+            color: Color(0xffb6b2b2),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _InnerTimeline extends StatelessWidget {
+  const _InnerTimeline({
+    required this.messages,
+  });
+
+  final List<_DeliveryMessage> messages;
+
+  @override
+  Widget build(BuildContext context) {
+    bool isEdgeIndex(int index) {
+      return index == 0 || index == messages.length + 1;
+    }
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: FixedTimeline.tileBuilder(
+        theme: TimelineTheme.of(context).copyWith(
+          nodePosition: 0,
+          connectorTheme: TimelineTheme.of(context).connectorTheme.copyWith(
+                thickness: 1.0,
+              ),
+          indicatorTheme: TimelineTheme.of(context).indicatorTheme.copyWith(
+                size: 10.0,
+                position: 0.5,
+              ),
+        ),
+        builder: TimelineTileBuilder(
+          indicatorBuilder: (_, index) =>
+              !isEdgeIndex(index) ? Indicator.outlined(borderWidth: 1.0) : null,
+          startConnectorBuilder: (_, index) => Connector.solidLine(),
+          endConnectorBuilder: (_, index) => Connector.solidLine(),
+          contentsBuilder: (_, index) {
+            if (isEdgeIndex(index)) {
+              return null;
+            }
+
+            return Padding(
+              padding: EdgeInsets.only(left: 8.0),
+              child: Text(messages[index - 1].toString()),
+            );
+          },
+          itemExtentBuilder: (_, index) => isEdgeIndex(index) ? 10.0 : 30.0,
+          nodeItemOverlapBuilder: (_, index) =>
+              isEdgeIndex(index) ? true : null,
+          itemCount: messages.length + 2,
+        ),
+      ),
+    );
+  }
+}
+
+class _DeliveryProcesses extends StatelessWidget {
+  const _DeliveryProcesses({Key? key, required this.processes})
+      : super(key: key);
+
+  final List<_DeliveryProcess> processes;
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTextStyle(
+      style: TextStyle(
+        color: Color(0xff9b9b9b),
+        fontSize: 12.5,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: FixedTimeline.tileBuilder(
+          theme: TimelineThemeData(
+            nodePosition: 0,
+            color: Color(0xff989898),
+            indicatorTheme: IndicatorThemeData(
+              position: 0,
+              size: 20.0,
+            ),
+            connectorTheme: ConnectorThemeData(
+              thickness: 2.5,
+            ),
+          ),
+          builder: TimelineTileBuilder.connected(
+            connectionDirection: ConnectionDirection.before,
+            itemCount: processes.length,
+            contentsBuilder: (_, index) {
+              if (processes[index].isCompleted) return null;
+
+              return Padding(
+                padding: EdgeInsets.only(left: 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      processes[index].name,
+                      style: DefaultTextStyle.of(context).style.copyWith(
+                            fontSize: 18.0,
+                          ),
+                    ),
+                    _InnerTimeline(messages: processes[index].messages),
+                  ],
+                ),
+              );
+            },
+            indicatorBuilder: (_, index) {
+              if (processes[index].isCompleted) {
+                return DotIndicator(
+                  color: Color(0xff66c97f),
+                  child: Icon(
+                    Icons.check,
+                    color: Colors.white,
+                    size: 12.0,
+                  ),
+                );
+              } else {
+                return OutlinedDotIndicator(
+                  borderWidth: 2.5,
+                );
+              }
+            },
+            connectorBuilder: (_, index, ___) => SolidLineConnector(
+              color: processes[index].isCompleted ? Color(0xff66c97f) : null,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _OnTimeBar extends StatelessWidget {
+  const _OnTimeBar({Key? key, required this.driver}) : super(key: key);
+
+  final _DriverInfo driver;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        MaterialButton(
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('On-time!'),
+              ),
+            );
+          },
+          elevation: 0,
+          shape: StadiumBorder(),
+          color: Color(0xff66c97f),
+          textColor: Colors.white,
+          child: Text('On-time'),
+        ),
+        Spacer(),
+        Text(
+          'Driver\n${driver.name}',
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(width: 12.0),
+        Container(
+          width: 40.0,
+          height: 40.0,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              fit: BoxFit.fitWidth,
+              image: NetworkImage(
+                driver.thumbnailUrl,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+_OrderInfo _data(int id) => _OrderInfo(
+      id: id,
+      date: DateTime.now(),
+      driverInfo: _DriverInfo(
+        name: 'Philipe',
+        thumbnailUrl:
+            'https://i.pinimg.com/originals/08/45/81/084581e3155d339376bf1d0e17979dc6.jpg',
+      ),
+      deliveryProcesses: [
+        _DeliveryProcess(
+          'Package Process',
+          messages: [
+            _DeliveryMessage('8:30am', 'Package received by driver'),
+            _DeliveryMessage('11:30am', 'Reached halfway mark'),
+          ],
+        ),
+        _DeliveryProcess(
+          'In Transit',
+          messages: [
+            _DeliveryMessage('13:00pm', 'Driver arrived at destination'),
+            _DeliveryMessage('11:35am', 'Package delivered by m.vassiliades'),
+          ],
+        ),
+        _DeliveryProcess.complete(),
+      ],
+    );
+
+class _OrderInfo {
+  const _OrderInfo({
+    required this.id,
+    required this.date,
+    required this.driverInfo,
+    required this.deliveryProcesses,
+  });
+
+  final int id;
+  final DateTime date;
+  final _DriverInfo driverInfo;
+  final List<_DeliveryProcess> deliveryProcesses;
+}
+
+class _DriverInfo {
+  const _DriverInfo({
+    required this.name,
+    required this.thumbnailUrl,
+  });
+
+  final String name;
+  final String thumbnailUrl;
+}
+
+class _DeliveryProcess {
+  const _DeliveryProcess(
+    this.name, {
+    this.messages = const [],
+  });
+
+  const _DeliveryProcess.complete()
+      : this.name = 'Done',
+        this.messages = const [];
+
+  final String name;
+  final List<_DeliveryMessage> messages;
+
+  bool get isCompleted => name == 'Done';
+}
+
+class _DeliveryMessage {
+  const _DeliveryMessage(this.createdAt, this.message);
+
+  final String createdAt; // final DateTime createdAt;
+  final String message;
+
+  @override
+  String toString() {
+    return '$createdAt $message';
   }
 }
