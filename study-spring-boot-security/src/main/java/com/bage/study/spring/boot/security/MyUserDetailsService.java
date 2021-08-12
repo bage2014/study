f2a59016dd502e697a6ad0e01b174509946e23af
WilliamSESE <87509674+WilliamSESE@users.noreply.github.com>
2021-08-12 20:18:58 +0800

    修改为更合适的注解@Service
    将类注解由@Component修改为@Service，该类主要承担业务逻辑处理，所以使用@Service注解更为合适。

--- study-spring-boot-security/src/main/java/com/bage/study/spring/boot/security/MyUserDetailsService.java
+++ study-spring-boot-security/src/main/java/com/bage/study/spring/boot/security/MyUserDetailsService.java
@@ -5,9 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class MyUserDetailsService implements UserDetailsService {
 
 	@Autowired
