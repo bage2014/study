a1c2a076def8fe4ef64d1ac7b7999431b32bc430
bage2014 <893542907@qq.com>
2021-10-27 12:42:14 +0800

    test fix

--- study-xfyun-lfasr/src/main/java/com/bage/study/xfyun/lfasr/JsonParser.java
+++ study-xfyun-lfasr/src/main/java/com/bage/study/xfyun/lfasr/JsonParser.java
@@ -1,22 +1,22 @@
-package com.bage.study.xfyun.lfasr;
-
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-
-public class JsonParser {
-
-	public static String parseMessage(String data) {
-		 JSONArray jsonArray = JSON.parseArray(data);
-		 StringBuilder sb = new StringBuilder();
-		 JSONObject item = null;
-		 for (int i = 0; i < jsonArray.size(); i++) {
-			 item = jsonArray.getJSONObject(i);
-			 if(item != null) {
-				 sb.append(item.getString("onebest"));
-			 }
-		}
-		return sb.toString();
-	}
-
-}
+//package com.bage.study.xfyun.lfasr;
+//
+//import com.alibaba.fastjson.JSON;
+//import com.alibaba.fastjson.JSONArray;
+//import com.alibaba.fastjson.JSONObject;
+//
+//public class JsonParser {
+//
+//	public static String parseMessage(String data) {
+//		 JSONArray jsonArray = JSON.parseArray(data);
+//		 StringBuilder sb = new StringBuilder();
+//		 JSONObject item = null;
+//		 for (int i = 0; i < jsonArray.size(); i++) {
+//			 item = jsonArray.getJSONObject(i);
+//			 if(item != null) {
+//				 sb.append(item.getString("onebest"));
+//			 }
+//		}
+//		return sb.toString();
+//	}
+//
+//}
