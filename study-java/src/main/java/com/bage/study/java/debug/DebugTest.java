d11a5827cbd64891cfd490dee3a2f94eddc7c7b0
bage <893542907@qq.com>
2022-01-18 22:07:24 +0800

    debug

---
+++ study-java/src/main/java/com/bage/study/java/debug/DebugTest.java
@@ -0,0 +1,19 @@
+package com.bage.study.java.debug;
+
+public class DebugTest {
+
+    public static void main(String[] args) {
+
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
+            // debug
+            // 先 选好 断点
+            // 在 右键 断点
+            // 设置停留条件即可
+        }
+        System.out.println("end ");
+
+    }
+
+}
