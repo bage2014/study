8f5be7973b0db39fb2a6afd80ebea173dd20c861
bage <893542907@qq.com>
2022-01-19 00:07:10 +0800

    jdk proxy

---
+++ study-java/src/main/java/com/bage/study/java/proxy/JdkProxyTest.java
@@ -0,0 +1,14 @@
+package com.bage.study.java.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+public class JdkProxyTest {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
