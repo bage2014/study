e7b2f1f5cb17a84c360bbcd68da3cdb97224d92e
bage <893542907@qq.com>
2022-03-10 00:13:37 +0800

    revert nodes

---
+++ study-algorithm/src/main/java/com/bage/study/algorithm/leetcode/RevertNodes.java
@@ -0,0 +1,39 @@
+package com.bage.study.algorithm.leetcode;
+
+import java.util.Objects;
+
+/**
+ * https://leetcode-cn.com/problems/UHnkqh/
+ */
+public class RevertNodes {
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(Objects.isNull(head) || Objects.isNull(head.next)){
+            return head;
+        }
+        ListNode listNode = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return listNode;
+    }
+}ß
+
+ class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
--- study-summary/README-bilibili.md
+++ study-summary/README-bilibili.md
@@ -2,6 +2,8 @@
 
 问答
 
+https://www.bilibili.com/video/BV1iV411p7LU?p=30&spm_id_from=pageDriver
+
 https://www.bilibili.com/video/BV1iV411p7LU?p=24&spm_id_from=pageDriver
 
 https://www.bilibili.com/video/BV1iV411p7LU?p=9&spm_id_from=pageDriver
