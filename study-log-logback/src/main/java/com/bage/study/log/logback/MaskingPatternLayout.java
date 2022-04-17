806d34041001b23c8c9c76616c4ae7ad4ef56d49
bage <893542907@qq.com>
2022-04-17 12:51:48 +0800

    log masking

---
+++ study-log-logback/src/main/java/com/bage/study/log/logback/MaskingPatternLayout.java
@@ -0,0 +1,42 @@
+package com.bage.study.log.logback;
+
+import ch.qos.logback.access.PatternLayout;
+import ch.qos.logback.access.spi.IAccessEvent;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class MaskingPatternLayout extends PatternLayout {
+
+    private Pattern multilinePattern;
+    private List<String> maskPatterns = new ArrayList<>();
+
+    public void addMaskPattern(String maskPattern) {
+        maskPatterns.add(maskPattern);
+        multilinePattern = Pattern.compile(maskPatterns.stream().collect(Collectors.joining("|")), Pattern.MULTILINE);
+    }
+
+    @Override
+    public String doLayout(IAccessEvent event) {
+        return super.doLayout(event);
+    }
+
+    private String maskMessage(String message) {
+        if (multilinePattern == null) {
+            return message;
+        }
+        StringBuilder sb = new StringBuilder(message);
+        Matcher matcher = multilinePattern.matcher(sb);
+        while (matcher.find()) {
+            IntStream.rangeClosed(1, matcher.groupCount()).forEach(group -> {
+                if (matcher.group(group) != null) {
+                    IntStream.range(matcher.start(group), matcher.end(group)).forEach(i -> sb.setCharAt(i, '*'));
+                }
+            });
+        }
+        return sb.toString();
+    }
+}
