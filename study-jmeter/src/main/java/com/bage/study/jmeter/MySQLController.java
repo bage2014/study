2a5d063659fa53b5010940ba12bb4a8bdb544bc4
bage <893542907@qq.com>
2022-05-04 01:59:36 +0800

    mysql pressure test

--- study-jmeter/src/main/java/com/bage/study/jmeter/MySQLController.java
+++ study-jmeter/src/main/java/com/bage/study/jmeter/MySQLController.java
@@ -6,20 +6,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.time.LocalDateTime;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/mysql")
 public class MySQLController {
 
-
+    Random random = new Random();
     @Resource
     private CustomerRepo repo;
 
     @RequestMapping("/query")
     public @ResponseBody
     Object query() {
-        System.out.println(LocalDateTime.now());
-        return repo.query();
+        String key = String.valueOf(System.currentTimeMillis()  - random.nextInt());
+        System.out.println(LocalDateTime.now() + "; " + key);
+        return repo.query(key);
     }
 
     @RequestMapping("/init")
