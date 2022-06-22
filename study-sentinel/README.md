e2b82f15c7ca8fce99793ab28cd3fd6d0367b884
bage <893542907@qq.com>
2022-06-22 12:48:57 +0800

    alibaba senital

--- study-sentinel/README.md
+++ study-sentinel/README.md
@@ -1,6 +1,13 @@
 
 ## study-sentinel
 官网    https://github.com/alibaba/Sentinel/wiki/%E4%BB%8B%E7%BB%8D
+<dependency>
+<groupId>com.alibaba.csp</groupId>
+<artifactId>sentinel-core</artifactId>
+<version>1.8.4</version>
+</dependency>
+
+
 
 demo https://github.com/alibaba/Sentinel/wiki/%E6%96%B0%E6%89%8B%E6%8C%87%E5%8D%97#%E5%85%AC%E7%BD%91-demo
 
--- study-sentinel/src/main/java/com/bage/App.java
+++ study-sentinel/src/main/java/com/bage/App.java
@@ -1,13 +1,50 @@
 package com.bage;
 
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        // 配置规则.
+        initFlowRules();
+
+        while (true) {
+            // 1.5.0 版本开始可以直接利用 try-with-resources 特性
+            try (Entry entry = SphU.entry("HelloWorld")) {
+                // 被保护的逻辑
+                System.out.println("hello world");
+            } catch (BlockException ex) {
+                // 处理被流控的逻辑
+                System.out.println("blocked!");
+            }
+        }
+    }
+
+    private static void initFlowRules() {
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("HelloWorld");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // Set limit QPS to 20.
+        rule.setCount(20);
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
+    }
+
+    @SentinelResource("HelloWorld")
+    public void helloWorld() {
+        // 资源中的逻辑
+        System.out.println("hello world");
     }
 }
