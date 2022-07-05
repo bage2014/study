8d46ad548131f1b56c45aa02dd7ccfbe8fb438d4
bage <893542907@qq.com>
2022-07-05 14:25:40 +0800

    limit test

---
+++ study-sentinel/src/main/java/com/bage/HelloSentinel.java
@@ -0,0 +1,53 @@
+package com.bage;
+
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Hello world!
+ */
+public class HelloSentinel {
+    public static void main(String[] args) {
+        // 配置规则.
+        initFlowRules();
+
+        int i = 0;
+        while (i < 100) {
+            System.out.print(i + ": ");
+            // 1.5.0 版本开始可以直接利用 try-with-resources 特性
+            try (Entry entry = SphU.entry("HelloWorld")) {
+                // 被保护的逻辑
+                System.out.println("hello world");
+            } catch (BlockException ex) {
+                // 处理被流控的逻辑
+                System.out.println("blocked!");
+            }
+            try {
+                Thread.sleep(100L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            i ++ ;
+        }
+    }
+
+    private static void initFlowRules() {
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("HelloWorld");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // Set limit QPS to 20.
+        rule.setCount(2);
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
+    }
+
+}
--- study-sentinel/src/main/java/com/bage/HelloWorld.java
+++ study-sentinel/src/main/java/com/bage/HelloWorld.java
@@ -14,7 +14,7 @@ import java.util.List;
 /**
  * Hello world!
  */
-public class App {
+public class HelloWorld {
     public static void main(String[] args) {
         // 配置规则.
         initFlowRules();
