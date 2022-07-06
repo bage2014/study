7c0441ec585d1fb8498493ce6dc6776a24755591
bage <893542907@qq.com>
2022-07-06 19:56:01 +0800

    sentinel

--- study-sentinel/README.md
+++ study-sentinel/README.md
@@ -7,6 +7,8 @@
 <version>1.8.4</version>
 </dependency>
 
+使用
+https://sentinelguard.io/zh-cn/docs/basic-api-resource-rule.html
 
 
 demo https://github.com/alibaba/Sentinel/wiki/%E6%96%B0%E6%89%8B%E6%8C%87%E5%8D%97#%E5%85%AC%E7%BD%91-demo
---
+++ study-sentinel/src/main/java/com/bage/HelloSentinelAnna.java
@@ -0,0 +1,48 @@
+package com.bage;
+
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Hello world!
+ */
+public class HelloSentinelAnna {
+    public static void main(String[] args) {
+        // 配置规则.
+        String hh = new HelloSentinelAnna().getUserById("hh");
+        
+        initFlowRules();
+    }
+
+    // 原本的业务方法.
+    @SentinelResource(blockHandler = "blockHandlerForGetUser")
+    public String getUserById(String id) {
+        throw new RuntimeException("getUserById command failed");
+    }
+
+    // blockHandler 函数，原方法调用被限流/降级/系统保护的时候调用
+    public String blockHandlerForGetUser(String id, BlockException ex) {
+        return "admin";
+    }
+
+    private static void initFlowRules() {
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("HelloWorld");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+//        rule.setGrade(RuleConstant.DEFAULT_WINDOW_INTERVAL_MS);
+        // Set limit QPS to 20.
+        rule.setCount(2);
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
+    }
+
+}
