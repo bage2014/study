0423d470f0b9872d24ee75ae43f35a2c9778b222
bage <893542907@qq.com>
2022-08-12 00:20:38 +0800

    user login

--- study-app-tutorials/lib/request/origin/login_result_mapping.dart
+++ study-app-tutorials/lib/request/origin/login_result_mapping.dart
@@ -1,11 +1,21 @@
+import 'dart:convert';
+
 import 'package:tutorials/component/http/http_result.dart';
+import 'package:tutorials/component/log/logs.dart';
 import 'package:tutorials/request/model/login/login_request_result.dart';
+import 'package:tutorials/request/origin/login_result.dart';
 
 class LoginResultMapping {
 
-  static LoginRequestResult mapping(HttpResult result) {
+  static LoginRequestResult mapping(HttpResult httpResult) {
+    Logs.info('httpResult ${httpResult.responseBody}');
 
-    return LoginRequestResult();
+    LoginResult loginResult = LoginResult.fromJson(jsonDecode(httpResult.responseBody));
+
+
+    LoginRequestResult result = LoginRequestResult();
+
+    return result;
   }
 
 }
