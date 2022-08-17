bdfa0eca2f3cfc4e882e76282f054d11fd0b74b0
bage <893542907@qq.com>
2022-08-17 14:52:26 +0800

    trace-server

--- study-spring-boot-trace-server/src/main/java/com/bage/AvailabilityController.java
+++ study-spring-boot-trace-server/src/main/java/com/bage/AvailabilityController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -13,22 +14,24 @@ import java.util.Set;
 public class AvailabilityController {
 
     private boolean validate(String console) {
-        return StringUtils.hasText(console) &&
-               Set.of("ps5", "ps4", "switch", "xbox").contains(console);
+        return "haha".equalsIgnoreCase(console);
+//        return StringUtils.hasText(console) &&
+//               Set.of("ps5", "ps4", "switch", "xbox").contains(console);
     }
 
     @GetMapping("/availability/{console}")
     Map<String, Object> getAvailability(@PathVariable String console) {
-        return Map.of("console", console,
-                "available", checkAvailability(console));
+        return new HashMap<String, Object>();
+//        return Map.of("console", console,
+//                "available", checkAvailability(console));
     }
 
-    private boolean checkAvailability(String console) {
-        Assert.state(validate(console), () -> "the console specified, " + console + ", is not valid.");
-        return switch (console) {
-            case "ps5" -> throw new RuntimeException("Service exception");
-            case "xbox" -> true;
-            default -> false;
-        };
-    }
+//    private boolean checkAvailability(String console) {
+//        Assert.state(validate(console), () -> "the console specified, " + console + ", is not valid.");
+//        return switch (console) {
+//            case "ps5" -> throw new RuntimeException("Service exception");
+//            case "xbox" -> true;
+//            default -> false;
+//        };
+//    }
 }
