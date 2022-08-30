1c06c7be23aaf1e5ff1463344c52865dc465d4d9
bage <893542907@qq.com>
2022-08-31 03:27:46 +0800

    web-socket

---
+++ study-spring-boot-web-socket/src/main/java/com/bage/GreetingController.java
@@ -0,0 +1,25 @@
+package com.bage;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+
+  @MessageMapping("/hello")
+  @SendTo("/topic/greetings")
+  public Greeting greeting(HelloMessage message) throws Exception {
+    Thread.sleep(1000); // simulated delay
+    return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+  }
+
+//  @RequestMapping("/")
+//  public String greeting() throws Exception {
+//    return "/index.html";
+//  }
+
+}
