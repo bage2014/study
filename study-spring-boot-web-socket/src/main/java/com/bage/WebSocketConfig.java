1ae056af175e65eef2967929310954e5c92c5ca5
bage <893542907@qq.com>
2022-09-06 15:11:59 +0800

    app context

--- study-spring-boot-web-socket/src/main/java/com/bage/WebSocketConfig.java
+++ study-spring-boot-web-socket/src/main/java/com/bage/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/").withSockJS();
+    registry.addEndpoint("/app").withSockJS();
   }
 
 }
--- study-spring-boot-web-socket/src/main/resources/static/app.js
+++ study-spring-boot-web-socket/src/main/resources/static/app.js
@@ -13,7 +13,7 @@ function setConnected(connected) {
 }
 
 function connect() {
-    var socket = new SockJS('/gs-guide-websocket');
+    var socket = new SockJS('/app');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         setConnected(true);
