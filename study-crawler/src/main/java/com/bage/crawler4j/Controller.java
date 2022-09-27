e43033e39ded79599519728a5cf3b156bf0d998c
bage <893542907@qq.com>
2022-09-27 14:36:35 +0800

    Update Controller.java

--- study-crawler/src/main/java/com/bage/crawler4j/Controller.java
+++ study-crawler/src/main/java/com/bage/crawler4j/Controller.java
@@ -27,11 +27,11 @@ public class Controller {
         controller.addSeed("https://www.ics.uci.edu/~welling/");
     	controller.addSeed("https://www.ics.uci.edu/");
     	
-    	// The factory which creates instances of crawlers.
-        CrawlController.WebCrawlerFactory<BasicCrawler> factory = MyCrawler::new;
-        
-        // Start the crawl. This is a blocking operation, meaning that your code
-        // will reach the line after this only when crawling is finished.
-        controller.start(factory, numberOfCrawlers);
+//    	// The factory which creates instances of crawlers.
+//        CrawlController.WebCrawlerFactory<BasicCrawler> factory = MyCrawler::new;
+//
+//        // Start the crawl. This is a blocking operation, meaning that your code
+//        // will reach the line after this only when crawling is finished.
+//        controller.start(factory, numberOfCrawlers);
     }
 }
