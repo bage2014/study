7fc59bb6a430da4ccd0aee29d5c8a0d5523f6b4e
bage <893542907@qq.com>
2022-09-28 14:42:03 +0800

    Create HHHH.java

---
+++ study-crawler/src/test/java/com/bage/HHHH.java
@@ -0,0 +1,13 @@
+package com.bage;
+
+import org.junit.Test;
+
+public class HHHH {
+
+    @Test
+    public void hello() {
+        System.out.println("JIA/XIAOJIN".split("/")[0]);
+// orderInfoSyncProcessor.process();
+    }
+
+}
