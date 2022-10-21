f01f9059b264d19c91544264c889b682998c113e
bage <893542907@qq.com>
2022-10-21 11:32:07 +0800

    test cse

--- study-app-tutorials/lib/view/settings/dev_tool.dart
+++ study-app-tutorials/lib/view/settings/dev_tool.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tutorials/component/cache/http_request_caches.dart';
-import 'package:tutorials/component/dialog/dialogs.dart';
-import 'package:tutorials/component/sp/shared_preference_helper.dart';
 import 'package:tutorials/constant/route_constant.dart';
-import 'package:tutorials/constant/sp_constant.dart';
 import 'package:tutorials/locale/translations.dart';
 
 class DevTool extends StatefulWidget {
