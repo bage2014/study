7075876c4b1796b84e2d070cfc42e766a67af1c9
bage <893542907@qq.com>
2022-10-27 22:37:27 +0800

    Create token_caches.dart

---
+++ study-app-tutorials/lib/component/cache/token_caches.dart
@@ -0,0 +1,21 @@
+
+import 'package:tutorials/request/model/user.dart';
+
+class TokenCaches{
+
+  static bool cacheAccessToken(String accessToken){
+    return true;
+  }
+
+  static bool clearAccessToken(String accessToken){
+    return true;
+  }
+
+  static bool cacheRefreshToken(String refreshToken){
+    return true;
+  }
+
+  static bool clearRefreshToken(String refreshToken){
+    return true;
+  }
+}
