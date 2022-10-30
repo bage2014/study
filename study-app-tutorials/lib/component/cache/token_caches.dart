0b4ac5e2d349d609bef8bbe9b86e6efad75e70bd
bage <893542907@qq.com>
2022-10-30 23:58:26 +0800

    login

--- study-app-tutorials/lib/component/cache/token_caches.dart
+++ study-app-tutorials/lib/component/cache/token_caches.dart
@@ -4,6 +4,10 @@ import 'package:tutorials/constant/sp_constant.dart';
 
 class TokenCaches{
 
+  static Future<String> getAccessToken(){
+    return SharedPreferenceHelper.get(SpConstant.token_access_key,'');
+  }
+  
   static Future<bool> cacheAccessToken(String accessToken){
     return SharedPreferenceHelper.set(SpConstant.token_access_key, accessToken);
   }
@@ -12,6 +16,10 @@ class TokenCaches{
     return SharedPreferenceHelper.set(SpConstant.token_access_key, '');
   }
 
+  static Future<String> getRefreshToken(){
+    return SharedPreferenceHelper.get(SpConstant.token_refresh_key,'');
+  }
+  
   static Future<bool> cacheRefreshToken(String refreshToken){
     return SharedPreferenceHelper.set(SpConstant.token_refresh_key, refreshToken);
   }
