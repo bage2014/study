cc50a581352d5a0fdeeda056ed2dbf97363902c4
bage <893542907@qq.com>
2022-12-06 15:24:27 +0800

    mybatis-plus

--- study-mybatis-plus/src/main/java/com/bage/Application.java
+++ study-mybatis-plus/src/main/java/com/bage/Application.java
@@ -25,27 +25,27 @@ public class Application implements CommandLineRunner {
     @Override
     public void run(String... strings) throws Exception {
 
-//        jdbcTemplate.execute("DROP TABLE IF EXISTS user;\n" +
-//                "\n" +
-//                "CREATE TABLE user\n" +
-//                "(\n" +
-//                "\tid BIGINT(20) NOT NULL COMMENT '主键ID',\n" +
-//                "\tname VARCHAR(30) NULL DEFAULT NULL COMMENT '姓名',\n" +
-//                "\tage INT(11) NULL DEFAULT NULL COMMENT '年龄',\n" +
-//                "\temail VARCHAR(50) NULL DEFAULT NULL COMMENT '邮箱',\n" +
-//                "\tPRIMARY KEY (id)\n" +
-//                ");" +
-//                "");
-//
-//
-//        jdbcTemplate.execute("DELETE FROM user;\n" +
-//                "\n" +
-//                "INSERT INTO user (id, name, age, email) VALUES\n" +
-//                "(1, 'Jone', 18, 'test1@baomidou.com'),\n" +
-//                "(2, 'Jack', 20, 'test2@baomidou.com'),\n" +
-//                "(3, 'Tom', 28, 'test3@baomidou.com'),\n" +
-//                "(4, 'Sandy', 21, 'test4@baomidou.com'),\n" +
-//                "(5, 'Billie', 24, 'test5@baomidou.com');");
+        jdbcTemplate.execute("DROP TABLE IF EXISTS user;\n" +
+                "\n" +
+                "CREATE TABLE user\n" +
+                "(\n" +
+                "\tid BIGINT(20) NOT NULL COMMENT '主键ID',\n" +
+                "\tname VARCHAR(30) NULL DEFAULT NULL COMMENT '姓名',\n" +
+                "\tage INT(11) NULL DEFAULT NULL COMMENT '年龄',\n" +
+                "\temail VARCHAR(50) NULL DEFAULT NULL COMMENT '邮箱',\n" +
+                "\tPRIMARY KEY (id)\n" +
+                ");" +
+                "");
+
+
+        jdbcTemplate.execute("DELETE FROM user;\n" +
+                "\n" +
+                "INSERT INTO user (id, name, age, email) VALUES\n" +
+                "(1, 'Jone', 18, 'test1@baomidou.com'),\n" +
+                "(2, 'Jack', 20, 'test2@baomidou.com'),\n" +
+                "(3, 'Tom', 28, 'test3@baomidou.com'),\n" +
+                "(4, 'Sandy', 21, 'test4@baomidou.com'),\n" +
+                "(5, 'Billie', 24, 'test5@baomidou.com');");
 
         System.out.println(("----- selectAll method test ------"));
         List<User> userList = userMapper.selectList(null);
