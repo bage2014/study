471a24fc08d3e5ba64f45aebf65d0afb86e2d487
bage <893542907@qq.com>
2022-12-28 12:42:23 +0800

    spring boot 3.1

---
+++ study-spring-boot-versions/src/main/java/com/bage/study/spring/boot/versions/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.bage.study.spring.boot.versions;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class DemoApplication {
+
+
+  public static void main(String[] args) {
+  SpringApplication.run(DemoApplication.class, args);
+  }
+
+  @GetMapping("/hello")
+  public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+  return String.format("Hello %s!", name);
+  }
+
+}
+            
