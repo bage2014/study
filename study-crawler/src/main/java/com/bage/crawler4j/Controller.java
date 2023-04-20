b01cf1c4f5a737214e1250f81ac75ec489fb42f1
bage <893542907@qq.com>
2023-04-20 16:39:40 +0800

    compile fix

--- study-crawler/src/main/java/com/bage/crawler4j/Controller.java
+++ study-crawler/src/main/java/com/bage/crawler4j/Controller.java
@@ -1,37 +1,37 @@
-package com.bage.crawler4j;
-
-import edu.uci.ics.crawler4j.crawler.CrawlConfig;
-import edu.uci.ics.crawler4j.crawler.CrawlController;
-import edu.uci.ics.crawler4j.fetcher.PageFetcher;
-import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
-import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
-
-public class Controller {
-    public static void main(String[] args) throws Exception {
-        String crawlStorageFolder = "/data/crawl/root";
-        int numberOfCrawlers = 7;
-
-        CrawlConfig config = new CrawlConfig();
-        config.setCrawlStorageFolder(crawlStorageFolder);
-
-        // Instantiate the controller for this crawl.
-        PageFetcher pageFetcher = new PageFetcher(config);
-        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
-        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
-        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
-
-        // For each crawl, you need to add some seed urls. These are the first
-        // URLs that are fetched and then the crawler starts following links
-        // which are found in these pages
-        controller.addSeed("https://www.ics.uci.edu/~lopes/");
-        controller.addSeed("https://www.ics.uci.edu/~welling/");
-    	controller.addSeed("https://www.ics.uci.edu/");
-    	
-//    	// The factory which creates instances of crawlers.
-//        CrawlController.WebCrawlerFactory<BasicCrawler> factory = MyCrawler::new;
+//package com.bage.crawler4j;
 //
-//        // Start the crawl. This is a blocking operation, meaning that your code
-//        // will reach the line after this only when crawling is finished.
-//        controller.start(factory, numberOfCrawlers);
-    }
-}
+//import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+//import edu.uci.ics.crawler4j.crawler.CrawlController;
+//import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+//import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+//import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+//
+//public class Controller {
+//    public static void main(String[] args) throws Exception {
+//        String crawlStorageFolder = "/data/crawl/root";
+//        int numberOfCrawlers = 7;
+//
+//        CrawlConfig config = new CrawlConfig();
+//        config.setCrawlStorageFolder(crawlStorageFolder);
+//
+//        // Instantiate the controller for this crawl.
+//        PageFetcher pageFetcher = new PageFetcher(config);
+//        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+//        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+//        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+//
+//        // For each crawl, you need to add some seed urls. These are the first
+//        // URLs that are fetched and then the crawler starts following links
+//        // which are found in these pages
+//        controller.addSeed("https://www.ics.uci.edu/~lopes/");
+//        controller.addSeed("https://www.ics.uci.edu/~welling/");
+//    	controller.addSeed("https://www.ics.uci.edu/");
+//
+////    	// The factory which creates instances of crawlers.
+////        CrawlController.WebCrawlerFactory<BasicCrawler> factory = MyCrawler::new;
+////
+////        // Start the crawl. This is a blocking operation, meaning that your code
+////        // will reach the line after this only when crawling is finished.
+////        controller.start(factory, numberOfCrawlers);
+//    }
+//}
--- study-resilience4j/src/main/java/com/bage/App.java
+++ study-resilience4j/src/main/java/com/bage/App.java
@@ -19,53 +19,53 @@ import java.util.function.Supplier;
  */
 public class App {
     public static void main(String[] args) {
-
-// Create a CircuitBreaker with default configuration
-        CircuitBreaker circuitBreaker = CircuitBreaker.ofDefaults("backendService");
-
-// Create a Retry with default configuration
-// 3 retry attempts and a fixed time interval between retries of 500ms
-        Retry retry = Retry.ofDefaults("backendService");
-
-// Create a Bulkhead with default configuration
-        Bulkhead bulkhead = Bulkhead.ofDefaults("backendService");
-
-        Supplier<String> supplier = () -> backendService
-                .doSomething(param1, param2);
-
-// Decorate your call to backendService.doSomething()
-// with a Bulkhead, CircuitBreaker and Retry
-// **note: you will need the resilience4j-all dependency for this
-        Supplier<String> decoratedSupplier = Decorators.ofSupplier(supplier)
-                .withCircuitBreaker(circuitBreaker)
-                .withBulkhead(bulkhead)
-                .withRetry(retry)
-                .decorate();
-
-// Execute the decorated supplier and recover from any exception
-        String result = Try.ofSupplier(decoratedSupplier)
-                .recover(throwable -> "Hello from Recovery").get();
-
-// When you don't want to decorate your lambda expression,
-// but just execute it and protect the call by a CircuitBreaker.
-        String result = circuitBreaker
-                .executeSupplier(backendService::doSomething);
-
-// You can also run the supplier asynchronously in a ThreadPoolBulkhead
-        ThreadPoolBulkhead threadPoolBulkhead = ThreadPoolBulkhead
-                .ofDefaults("backendService");
-
-// The Scheduler is needed to schedule a timeout on a non-blocking CompletableFuture
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(3);
-        TimeLimiter timeLimiter = TimeLimiter.of(Duration.ofSeconds(1));
-
-        CompletableFuture<String> future = Decorators.ofSupplier(supplier)
-                .withThreadPoolBulkhead(threadPoolBulkhead)
-                .withTimeLimiter(timeLimiter, scheduler)
-                .withCircuitBreaker(circuitBreaker)
-                .withFallback(asList(TimeoutException.class, CallNotPermittedException.class, BulkheadFullException.class),
-                        throwable -> "Hello from Recovery")
-                .get().toCompletableFuture();
+//
+//// Create a CircuitBreaker with default configuration
+//        CircuitBreaker circuitBreaker = CircuitBreaker.ofDefaults("backendService");
+//
+//// Create a Retry with default configuration
+//// 3 retry attempts and a fixed time interval between retries of 500ms
+//        Retry retry = Retry.ofDefaults("backendService");
+//
+//// Create a Bulkhead with default configuration
+//        Bulkhead bulkhead = Bulkhead.ofDefaults("backendService");
+//
+//        Supplier<String> supplier = () -> backendService
+//                .doSomething(param1, param2);
+//
+//// Decorate your call to backendService.doSomething()
+//// with a Bulkhead, CircuitBreaker and Retry
+//// **note: you will need the resilience4j-all dependency for this
+//        Supplier<String> decoratedSupplier = Decorators.ofSupplier(supplier)
+//                .withCircuitBreaker(circuitBreaker)
+//                .withBulkhead(bulkhead)
+//                .withRetry(retry)
+//                .decorate();
+//
+//// Execute the decorated supplier and recover from any exception
+//        String result = Try.ofSupplier(decoratedSupplier)
+//                .recover(throwable -> "Hello from Recovery").get();
+//
+//// When you don't want to decorate your lambda expression,
+//// but just execute it and protect the call by a CircuitBreaker.
+//        String result = circuitBreaker
+//                .executeSupplier(backendService::doSomething);
+//
+//// You can also run the supplier asynchronously in a ThreadPoolBulkhead
+//        ThreadPoolBulkhead threadPoolBulkhead = ThreadPoolBulkhead
+//                .ofDefaults("backendService");
+//
+//// The Scheduler is needed to schedule a timeout on a non-blocking CompletableFuture
+//        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(3);
+//        TimeLimiter timeLimiter = TimeLimiter.of(Duration.ofSeconds(1));
+//
+//        CompletableFuture<String> future = Decorators.ofSupplier(supplier)
+//                .withThreadPoolBulkhead(threadPoolBulkhead)
+//                .withTimeLimiter(timeLimiter, scheduler)
+//                .withCircuitBreaker(circuitBreaker)
+//                .withFallback(asList(TimeoutException.class, CallNotPermittedException.class, BulkheadFullException.class),
+//                        throwable -> "Hello from Recovery")
+//                .get().toCompletableFuture();
 
     }
 }
