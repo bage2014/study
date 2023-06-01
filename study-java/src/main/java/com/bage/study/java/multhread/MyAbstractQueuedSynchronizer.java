a4d294990a06ced5641c6202b81a45ed6d1f3a6a
bage <893542907@qq.com>
2023-06-02 01:39:00 +0800

    AQS

--- study-java/src/main/java/com/bage/study/java/multhread/MyAbstractQueuedSynchronizer.java
+++ study-java/src/main/java/com/bage/study/java/multhread/MyAbstractQueuedSynchronizer.java
@@ -1,10 +1,16 @@
 package com.bage.study.java.multhread;
 
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
 /**
  * AQS
  * @author bage
  *
  */
-public class MyAbstractQueuedSynchronizer {
+public class MyAbstractQueuedSynchronizer extends AbstractQueuedSynchronizer {
 
+    @Override
+    protected boolean tryAcquire(int arg) {
+        return super.tryAcquire(arg);
+    }
 }
