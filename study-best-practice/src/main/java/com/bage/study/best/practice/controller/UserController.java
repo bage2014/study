b31eb9f72bffe9df982b8e49d007e07be5c2b3c5
bage <893542907@qq.com>
2023-06-07 01:02:01 +0800

    jmeter insert

--- study-best-practice/src/main/java/com/bage/study/best/practice/controller/UserController.java
+++ study-best-practice/src/main/java/com/bage/study/best/practice/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.bage.study.best.practice.controller;
 
 import com.bage.study.best.practice.model.User;
+import com.bage.study.best.practice.service.UserMockService;
 import com.bage.study.best.practice.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,13 +18,24 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserMockService userMockService;
 
     @RequestMapping("/query")
     public Object query(@RequestParam("phone") String phone) {
         log.info("UserController query phone = {}", phone);
-        List<User> users = userService.queryUser(phone);
+        List<User> users = userService.query(phone);
         log.info("UserController query users = {}", users);
         return users;
+    }
+
+    @RequestMapping("/insert")
+    public Object insert() {
+        User user = userMockService.mockOne();
+        log.info("UserController insert user = {}", user);
+        int insert = userService.insert(user);
+        log.info("UserController insert insert = {}", insert);
+        return insert;
     }
 
 }
---
+++ study-best-practice/src/main/java/com/bage/study/best/practice/service/UserMockService.java
@@ -0,0 +1,7 @@
+package com.bage.study.best.practice.service;
+
+import com.bage.study.best.practice.model.User;
+
+public interface UserMockService {
+    User mockOne();
+}
