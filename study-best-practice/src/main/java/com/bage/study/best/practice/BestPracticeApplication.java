b089a1010b79e398cddd9233df772f738cfff53c
bage <893542907@qq.com>
2023-06-08 08:36:59 +0800

    mongo db

--- study-best-practice/src/main/java/com/bage/study/best/practice/BestPracticeApplication.java
+++ study-best-practice/src/main/java/com/bage/study/best/practice/BestPracticeApplication.java
@@ -1,15 +1,9 @@
 package com.bage.study.best.practice;
 
-import com.bage.study.best.practice.service.mock.UserServiceMockImpl;
-import com.bage.study.best.practice.repo.UserMapper;
-import com.bage.study.best.practice.repo.UserEntity;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.Resource;
-import java.util.List;
 
 @SpringBootApplication
 @MapperScan("com.bage.study.best.practice.repo")
