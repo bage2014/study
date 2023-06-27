ccac575e8bb391c8f942e16520f9ec13bad360ee
bage <893542907@qq.com>
2023-06-27 18:41:35 +0800

    oracle open jmh

---
+++ study-jmh/README.md
@@ -0,0 +1,32 @@
+# study-jmh
+
+
+
+## 参考链接
+
+GitHub 
+https://github.com/openjdk/jmh/tree/master/jmh-samples/src/main/java/org/openjdk/jmh/samples
+
+官网文档 
+https://openjdk.org/projects/code-tools/jmh/
+
+## 快速开始
+
+http://imyhq.com/software-engineering/4517.html
+
+https://cloud.tencent.com/developer/article/2100320
+
+https://juejin.cn/post/6990031829071822856
+
+
+## 应用实践
+
+下载运行
+
+```
+
+```
+
+## 原理解析
+
+基本原理
---
+++ study-jmh/src/main/java/com/bage/study/jmh/JMHSample_01_HelloWorld.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2014, Oracle America, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Oracle nor the names of its contributors may be used
+ *    to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.bage.study.jmh;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+public class JMHSample_01_HelloWorld {
+
+    /*
+     * This is our first benchmark method.
+     *
+     * JMH works as follows: users annotate the methods with @Benchmark, and
+     * then JMH produces the generated code to run this particular benchmark as
+     * reliably as possible. In general one might think about @Benchmark methods
+     * as the benchmark "payload", the things we want to measure. The
+     * surrounding infrastructure is provided by the harness itself.
+     *
+     * Read the Javadoc for @Benchmark annotation for complete semantics and
+     * restrictions. At this point we only note that the methods names are
+     * non-essential, and it only matters that the methods are marked with
+     * @Benchmark. You can have multiple benchmark methods within the same
+     * class.
+     *
+     * Note: if the benchmark method never finishes, then JMH run never finishes
+     * as well. If you throw an exception from the method body the JMH run ends
+     * abruptly for this benchmark and JMH will run the next benchmark down the
+     * list.
+     *
+     * Although this benchmark measures "nothing" it is a good showcase for the
+     * overheads the infrastructure bear on the code you measure in the method.
+     * There are no magical infrastructures which incur no overhead, and it is
+     * important to know what are the infra overheads you are dealing with. You
+     * might find this thought unfolded in future examples by having the
+     * "baseline" measurements to compare against.
+     */
+
+    @Benchmark
+    public void wellHelloThere() {
+        // this method was intentionally left blank.
+    }
+
+    /*
+     * ============================== HOW TO RUN THIS TEST: ====================================
+     *
+     * You are expected to see the run with large number of iterations, and
+     * very large throughput numbers. You can see that as the estimate of the
+     * harness overheads per method call. In most of our measurements, it is
+     * down to several cycles per call.
+     *
+     * a) Via command-line:
+     *    $ mvn clean install
+     *    $ java -jar target/benchmarks.jar JMHSample_01
+     *
+     * JMH generates self-contained JARs, bundling JMH together with it.
+     * The runtime options for the JMH are available with "-h":
+     *    $ java -jar target/benchmarks.jar -h
+     *
+     * b) Via the Java API:
+     *    (see the JMH homepage for possible caveats when running from IDE:
+     *      http://openjdk.java.net/projects/code-tools/jmh/)
+     */
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(JMHSample_01_HelloWorld.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+}
