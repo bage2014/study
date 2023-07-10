33f63289484f3ea693187f04308bb0705efd7faa
bage <893542907@qq.com>
2023-07-11 00:53:19 +0800

    feature

--- study-java/src/main/java/com/bage/study/java/multhread/FeatureTest.java
+++ study-java/src/main/java/com/bage/study/java/multhread/FeatureTest.java
@@ -1,4 +1,21 @@
 package com.bage.study.java.multhread;
 
+import java.util.concurrent.*;
+
 public class FeatureTest {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        FutureTask<String> future = new FutureTask<String>(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                Thread.sleep(2000);
+                return "Callable hello world";
+            }
+        });
+        new Thread(future).start();
+        String result = future.get();
+        System.out.println(result);
+
+    }
+
 }
