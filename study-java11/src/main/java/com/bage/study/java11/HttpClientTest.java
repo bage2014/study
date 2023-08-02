43b826cd8bb1dd457d9e1a71ce2083d8d13b16b7
bage <893542907@qq.com>
2023-08-03 00:05:29 +0800

    Update HttpClientTest.java

--- study-java11/src/main/java/com/bage/study/java11/HttpClientTest.java
+++ study-java11/src/main/java/com/bage/study/java11/HttpClientTest.java
@@ -1,13 +1,27 @@
 package com.bage.study.java11;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Duration;
 
+/**
+ * https://docs.oracle.com/en/java/javase/11/docs/api/java.net.http/java/net/http/HttpClient.html
+ *
+ */
 public class HttpClientTest {
 
     public static void main(String[] args) throws Exception {
+
+
+//        basic();
+
+        async();
+    }
+
+    private static void basic() throws IOException, InterruptedException {
         HttpClient build = HttpClient.newBuilder()
 //                .proxy() // 代理设置
                 .build();
@@ -17,8 +31,25 @@ public class HttpClientTest {
                 .build();
         HttpResponse<String> send = build.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(send.body());
-
-
     }
+
+    private static void async() throws InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://github.com/bage2014/study/tree/master/study-todo"))
+                .timeout(Duration.ofMinutes(2))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString("{}"))
+//                .POST(HttpRequest.BodyPublishers.ofFile(Paths.get("file.json")))
+                .build();
+        HttpClient client = HttpClient.newBuilder()
+//                .proxy() // 代理设置
+                .build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .whenComplete((response,throwable)->{
+                    System.out.println(response.body());
+                });
+        Thread.sleep(3000L);
+    }
+
 
 }
