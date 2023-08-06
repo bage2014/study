ca881e8cd8cfa7c448596fe1c72aae88aa6bf0e1
bage <893542907@qq.com>
2023-08-06 11:01:41 +0800

    two sum

---
+++ study-algorithm/src/main/java/com/bage/study/algorithm/leetcode/twosum/Solution.java
@@ -0,0 +1,48 @@
+package com.bage.study.algorithm.leetcode.twosum;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.cn/problems/two-sum/
+ */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+    public int[] twoSumAdvance(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int remain = target - nums[i];
+            Integer index = map.get(remain);
+
+            if (index != null && index != i) {
+                return new int[]{i, index};
+            }
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(new Solution().twoSum(nums, target)));
+    }
+}
