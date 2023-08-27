10f41a2d965185c9d724eef8300caeedf6a9e0f7
bage <893542907@qq.com>
2023-08-27 14:19:38 +0800

    java gc

---
+++ study-gc/study-gc-jdk8/src/main/java/com/bage/study/gc/jdk8/App.java
@@ -0,0 +1,13 @@
+package com.bage.study.gc.jdk8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
