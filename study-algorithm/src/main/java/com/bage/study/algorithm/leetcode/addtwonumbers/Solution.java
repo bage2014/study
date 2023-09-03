c933968683bf47c8f6c7f5e770fad6046c912de7
bage <893542907@qq.com>
2023-09-03 11:33:24 +0800

    ListNode

---
+++ study-algorithm/src/main/java/com/bage/study/algorithm/leetcode/addtwonumbers/Solution.java
@@ -0,0 +1,40 @@
+package com.bage.study.algorithm.leetcode.addtwonumbers;
+
+import com.bage.study.algorithm.leetcode.common.ListNode;
+import com.bage.study.algorithm.leetcode.common.NodeUtils;
+
+/**
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ * <p>
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * <p>
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * <p>
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head1 = l1;
+        ListNode head2 = l2;
+//        while(){
+//
+//        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = NodeUtils.init(5,4,3);
+        ListNode l2 = NodeUtils.init(5,4,3);
+        ListNode listNode = new Solution().addTwoNumbers(l1, l2);
+        NodeUtils.print(listNode);
+    }
+
+}
+
---
+++ study-algorithm/src/main/java/com/bage/study/algorithm/leetcode/common/ListNode.java
@@ -0,0 +1,18 @@
+package com.bage.study.algorithm.leetcode.common;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
