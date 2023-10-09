1b67408e0213c734876eece4a7c2b125e572c6e6
bage <893542907@qq.com>
2023-10-09 09:08:05 +0800

    virtual thread

--- study-java21/src/main/java/com/bage/jdk21/virtual/SimpleThread.java
+++
@@ -1,8 +0,0 @@
-package com.bage.jdk21.virtual;
-
-public class SimpleThread implements Runnable{
-    @Override
-    public void run() {
-        System.out.println("SimpleThread-bage-hhhh-");
-    }
-}
