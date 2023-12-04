8892497f01e6e74f833a1566c4942dd81d306f4c
bage <893542907@qq.com>
2023-12-04 12:50:50 +0800

    spring custmer validation

--- study-spring-boot-valid/README.md
+++ study-spring-boot-valid/README.md
@@ -7,3 +7,22 @@ https://spring.io/guides/gs/validating-form-input/
 https://cloud.tencent.com/developer/article/1193178?from=15425
 
 
+
+自定义校验
+
+https://juejin.cn/post/7295540969407250441
+
+https://www.zhihu.com/question/589827662?utm_id=0
+
+
+
+访问验证
+
+http://localhost:8080/
+
+
+
+
+
+
+
---
+++ study-spring-boot-valid/src/main/java/com/bage/spring/boot/valid/CustomValidator.java
@@ -0,0 +1,13 @@
+package com.bage.spring.boot.valid;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+public class CustomValidator implements ConstraintValidator<CustomValidation, String> {
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        // 在这里编写自定义校验逻辑
+        System.out.println("CustomValidator:" + value);
+        return value != null && value.startsWith("abc");
+    }
+}
+
