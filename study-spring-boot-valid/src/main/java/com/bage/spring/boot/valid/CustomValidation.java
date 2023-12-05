2636ede3f33fd21da6d95b4f524480acee8221dd
bage <893542907@qq.com>
2023-12-05 13:24:43 +0800

    spring validate

--- study-spring-boot-valid/src/main/java/com/bage/spring/boot/valid/CustomValidation.java
+++ study-spring-boot-valid/src/main/java/com/bage/spring/boot/valid/CustomValidation.java
@@ -4,6 +4,10 @@ import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.*;
 
+/**
+ * groups这里主要进行将validator进行分类，不同的类group中会执行不同的validator操作
+ * payload 主要是针对bean的，使用不多。
+ */
 @Documented
 @Constraint(validatedBy = CustomValidator.class)
 @Target( { ElementType.METHOD, ElementType.FIELD })
@@ -14,6 +18,10 @@ public @interface CustomValidation {
 
     Class<?>[] groups() default {};
 
+    /**
+     * Payload
+     * @return
+     */
     Class<? extends Payload>[] payload() default {};
 
 }
