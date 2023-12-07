74da976908f59070d69564376f7926703e1b962e
bage <893542907@qq.com>
2023-12-07 12:41:11 +0800

    pac4j

---
+++ study-pac4j/README.md
@@ -0,0 +1,17 @@
+
+# study-pac4j
+
+
+## 参考链接
+
+https://github.com/pac4j/pac4j
+
+
+https://juejin.cn/post/7308782851360555018
+
+
+## 访问链接 
+http://localhost:8080/
+
+
+
---
+++ study-pac4j/src/main/java/com/bage/study/pac4j/IndexController.java
@@ -0,0 +1,13 @@
+package com.bage.study.pac4j;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index ok";
+    }
+}
