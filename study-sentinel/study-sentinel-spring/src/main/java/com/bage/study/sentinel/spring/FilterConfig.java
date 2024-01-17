961f6a5cb8d7de0be9187b9685f7229a8b4a9b6d
bage <893542907@qq.com>
2024-01-17 13:12:59 +0800

    sentinel spring web

---
+++ study-sentinel/study-sentinel-spring/src/main/java/com/bage/study/sentinel/spring/FilterConfig.java
@@ -0,0 +1,23 @@
+package com.bage.study.sentinel.spring;
+
+import com.alibaba.csp.sentinel.adapter.servlet.CommonFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean sentinelFilterRegistration() {
+        FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new CommonFilter());
+        registration.addUrlPatterns("/*");
+        registration.setName("sentinelFilter");
+        registration.setOrder(1);
+
+        return registration;
+    }
+}
