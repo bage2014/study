0f89de4ae4e2fd2ec67e53ff77ca80cae4cd1ec7
bage <893542907@qq.com>
2024-02-04 20:34:46 +0800

    aop

---
+++ bage-spring-boot-starter-ping/study-spring-boot-dev-tools/src/main/java/com/bage/study/spring/boot/dev/tools/DevToolsApplication.java
@@ -0,0 +1,13 @@
+package com.bage.study.spring.boot.dev.tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevToolsApplication.class, args);
+	}
+
+}
---
+++ bage-spring-boot-starter-ping/study-spring-boot-dev-tools/src/main/java/com/bage/study/spring/boot/dev/tools/HelloController.java
@@ -0,0 +1,13 @@
+package com.bage.study.spring.boot.dev.tools;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello12222";
+    }
+}
