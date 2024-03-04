34de300ad7c20d2ee23bb2e19c323a8d657c4eac
bage <893542907@qq.com>
2024-03-04 08:49:44 +0800

    two sum

--- study-algorithm/src/main/java/com/bage/study/algorithm/top100/TwoSum.java
+++ study-algorithm/src/main/java/com/bage/study/algorithm/top100/TwoSum.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * https://leetcode.cn/problems/two-sum/description/
+ */
 public class TwoSum {
     public static void main(String[] args) {
         int[] ints = new Solution().twoSum(new int[]{2,7,11,15}, 9);
@@ -14,25 +17,26 @@ public class TwoSum {
         System.out.println(Arrays.toString(ints));
     }
 
-}
 
+    static class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            Map<Integer,Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                int num = nums[i];
+                map.put(num,i);
+            }
 
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
-            map.put(num,i);
+            for (int i = 0; i < nums.length; i++) {
+                int leftNum = target - nums[i];
+                int index = map.getOrDefault(leftNum,-1);
+                if(index >= 0 && index != i){
+                    return new int[]{i,index};
+                };
+            }
+            return new int[0];
         }
-
-        for (int i = 0; i < nums.length; i++) {
-            int leftNum = target - nums[i];
-            int index = map.getOrDefault(leftNum,-1);
-            if(index >= 0 && index != i){
-                return new int[]{i,index};
-            };
-        }
-        return new int[0];
     }
+
 }
+
 
