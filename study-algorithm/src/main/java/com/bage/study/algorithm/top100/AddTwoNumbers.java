62586ece506d940323e15f6e2f6435fa2265a153
bage <893542907@qq.com>
2024-03-06 08:00:37 +0800

    add two sum with linkednode

--- study-algorithm/src/main/java/com/bage/study/algorithm/top100/AddTwoNumbers.java
+++ study-algorithm/src/main/java/com/bage/study/algorithm/top100/AddTwoNumbers.java
@@ -9,9 +9,22 @@ import com.bage.study.algorithm.common.ListNodeUtils;
 public class AddTwoNumbers {
 
     public static void main(String[] args) {
-       ListNode node1 = ListNodeUtils.init(2,4,3);
-       ListNode node2 = ListNodeUtils.init(5,6,4);
-       ListNode listNode = new Solution().addTwoNumbers(node1, node2);
+        ListNode node1 = null;
+        ListNode node2 = null;
+        ListNode listNode = null;
+//      node1 = ListNodeUtils.init(2,4,3);
+//      node2 = ListNodeUtils.init(5,6,4);
+//      listNode = new Solution().addTwoNumbers(node1, node2);
+//       ListNodeUtils.print(listNode);
+//
+//       node1 = ListNodeUtils.init(2,4,3);
+//       node2 = ListNodeUtils.init(5,6,7);
+//       listNode = new Solution().addTwoNumbers(node1, node2);
+//       ListNodeUtils.print(listNode);
+
+       node1 = ListNodeUtils.init(2,4,3);
+       node2 = ListNodeUtils.init(5,6);
+       listNode = new Solution().addTwoNumbers(node1, node2);
        ListNodeUtils.print(listNode);
     }
 
@@ -32,29 +45,45 @@ public class AddTwoNumbers {
             ListNode node1 = l1;
             ListNode node2 = l2;
             ListNode result = null; // 结果
-            ListNode last = result; // 结果
+            ListNode last = null; // 结果的最后一位
             int flag = 0; // 进位
 
-            while (node1 != null && node2 != null){
+            while (true){
                 int current = node1.val + node2.val + flag;
 
                 flag = current >= 10 ? 1 : 0;
                 current = current % 10;
 
                 ListNode newOne = new ListNode(current);
-                if(result == null){
+                if(result == null){ // 初始化
                     result = newOne;
                     last = newOne;
                 } else {
                     last.next = newOne;
-                    last = last.next;
+                    last = last.next; // 往后移动
                 }
 
                 node1 = node1.next;
                 node2 = node2.next;
+
+                if(node1 == null && node2 == null){
+                    break;
+                }
+
+                if(node1 == null && node2 != null){
+                    node1 = new ListNode(0);
+                }
+                if(node2 == null && node1 != null){
+                    node2 = new ListNode(0);
+                }
             }
 
 
+            if(flag == 1){
+                ListNode newOne = new ListNode(flag);
+                last.next = newOne;
+                last = last.next; // 往后移动
+            }
             return result;
         }
     }
