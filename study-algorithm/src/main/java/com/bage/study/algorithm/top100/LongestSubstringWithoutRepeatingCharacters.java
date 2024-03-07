57ba2ac360a512371e51e2e54dff05068e20b1bd
bage <893542907@qq.com>
2024-03-07 12:40:41 +0800

    Create LongestSubstringWithoutRepeatingCharacters.java

---
+++ study-algorithm/src/main/java/com/bage/study/algorithm/top100/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,25 @@
+package com.bage.study.algorithm.top100;
+
+/**
+ * https://leetcode.cn/problems/longest-substring-without-repeating-characters/description/
+ *
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().lengthOfLongestSubstring("abcabcbb"));
+    }
+
+    /**
+     * 输入: s = "abcabcbb"
+     * 输出: 3
+     * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+     */
+   static class Solution {
+        public int lengthOfLongestSubstring(String str) {
+            return 0;
+        }
+
+    }
+
+}
