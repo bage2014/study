13b6842457adfc12eb2f12ccfcd9b8031b37f9e1
bage <893542907@qq.com>
2024-03-11 08:52:33 +0800

    merge sorted arrays

--- study-algorithm/src/main/java/com/bage/study/algorithm/top100/MedianOfTwoSortedArrays.java
+++ study-algorithm/src/main/java/com/bage/study/algorithm/top100/MedianOfTwoSortedArrays.java
@@ -1,6 +1,24 @@
 package com.bage.study.algorithm.top100;
 
+/**
+ * https://leetcode.cn/problems/median-of-two-sorted-arrays/
+ *
+ */
 public class MedianOfTwoSortedArrays {
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{1,3};
+        int[] nums2 = new int[]{2};
+        double medianSortedArrays = new MedianOfTwoSortedArrays().findMedianSortedArrays(nums1,nums2);
+        System.out.println(medianSortedArrays);
+
+
+        nums1 = new int[]{1,2};
+        nums2 = new int[]{3,4};
+        medianSortedArrays = new MedianOfTwoSortedArrays().findMedianSortedArrays(nums1,nums2);
+        System.out.println(medianSortedArrays);
+
+
+    }
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         if (nums1.length == 0) {
             return findMedianSortedArrays(nums2);
@@ -9,44 +27,33 @@ public class MedianOfTwoSortedArrays {
             return findMedianSortedArrays(nums1);
         }
         int total = nums1.length + nums2.length;
-        int middleIndex = (total + 1) / 2; // +1 保证基数下能多一次执行  == 2
+        int numsAll[] = new int[total];
 
         int nums1Index = 0;
         int nums2Index = 0;
-
-        int count = 0;
-        int currentValue = -1;
-        int nextValue = -1;
-        while (count < middleIndex) { // 找到中间位置
+        for (int i = 0; i < total; i++) {
             if (nums1[nums1Index] <= nums2[nums2Index]) {
-                currentValue = nums1[nums1Index];
-                if (nums1Index < nums1.length - 1) {
-                    // nums1 比较小 并且 index1 还能移动
-                    nums1Index++;
-                    nextValue = nums1[nums1Index] <= nums2[nums2Index] ? nums1[nums1Index]:nums2[nums2Index];
-
-                } else {
-                    // nums1 比较小 但是 index1 还能移动，只能移动 index2
-                    nextValue = nums2[nums2Index];
-                    nums2Index++;
-                }
+                numsAll[i] = nums1[nums1Index];
+                nums1Index = nums1Index+1;
             } else {
-                currentValue = nums2[nums2Index];
-                if (nums2Index < nums2.length - 1) {
-                    nums2Index++;
-                    nextValue = nums2[nums2Index] <= nums1[nums1Index] ? nums2[nums2Index]:nums1[nums1Index];
-                } else {
-                    nextValue = nums1[nums1Index];
-                    nums1Index++;
-                }
+                numsAll[i] = nums2[nums2Index];
+                nums2Index = nums2Index+1;
             }
-            count++;
+            if(nums1Index >= nums1.length && nums2Index < nums2.length) {
+                append(nums2,nums2Index,i+1,numsAll);
+                break;
+            } else if(nums2Index >= nums2.length && nums1Index < nums1.length){
+                append(nums1,nums1Index,i+1,numsAll);
+                break;
+            }
         }
-        if (total % 2 == 1) {
-            return currentValue;
+        return findMedianSortedArrays(numsAll);
+    }
+
+    private void append(int[] source, int sourceCurrent, int index, int[] numsAll) {
+        for (int i = 0; i < source.length - sourceCurrent; i++) {
+            numsAll[index+i] = source[sourceCurrent+i];
         }
-        // 偶数
-        return (currentValue + nextValue) / 2.0;
     }
 
     private double findMedianSortedArrays(int[] nums) {
