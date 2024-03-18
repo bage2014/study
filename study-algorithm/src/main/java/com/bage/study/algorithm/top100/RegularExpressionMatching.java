8824ae34873a76214f08f011054991a4eaf6ff0f
bage <893542907@qq.com>
2024-03-18 09:12:32 +0800

    RegularExpressionMatching

--- study-algorithm/src/main/java/com/bage/study/algorithm/top100/RegularExpressionMatching.java
+++ study-algorithm/src/main/java/com/bage/study/algorithm/top100/RegularExpressionMatching.java
@@ -22,13 +22,25 @@ public class RegularExpressionMatching {
         while (true){
             char charAtStr = str.charAt(indexStr);
             char charAtPattern = pattern.charAt(indexPattern);
+            char charAtPatternNext = charAtNext(indexPattern,pattern);
 
             if(charAtPattern == '.'){
+                if(charAtPatternNext == '*'){
+                    // 后续不用在看，肯定匹配
+                    return true;
+                }
+                // else 相当于匹配
                 indexStr ++;
                 indexPattern ++;
             } else if(charAtPattern == '*'){
                 indexStr ++;
                 indexPattern ++;
+            } else {
+                // 其他情况，比如普通字符，， ABCD
+                indexStr ++;
+                if(charAtPatternNext == '*'){
+                    indexPattern ++;
+                }
             }
 
             if(indexStr > str.length() || indexPattern < pattern.length()){
@@ -37,6 +49,13 @@ public class RegularExpressionMatching {
         }
 
         return false;
+    }
+
+    private char charAtNext(int indexPattern, String pattern) {
+        if(indexPattern >= pattern.length()){
+            return ' ';
+        }
+        return pattern.charAt(indexPattern + 1);
     }
 
 }
