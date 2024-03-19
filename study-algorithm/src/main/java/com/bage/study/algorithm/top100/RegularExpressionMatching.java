1cff7b6fe083ebbb2d47d7a7003e48793b700a4c
bage <893542907@qq.com>
2024-03-19 08:05:14 +0800

    Update RegularExpressionMatching.java

--- study-algorithm/src/main/java/com/bage/study/algorithm/top100/RegularExpressionMatching.java
+++ study-algorithm/src/main/java/com/bage/study/algorithm/top100/RegularExpressionMatching.java
@@ -5,6 +5,13 @@ package com.bage.study.algorithm.top100;
  */
 public class RegularExpressionMatching {
 
+    public static void main(String[] args) {
+        System.out.println(new RegularExpressionMatching().isMatch("aa","aa"));
+        System.out.println(new RegularExpressionMatching().isMatch("aa","a"));
+        System.out.println(new RegularExpressionMatching().isMatch("ab","a*"));
+        System.out.println(new RegularExpressionMatching().isMatch("ab",".*"));
+    }
+
     public boolean isMatch(String str, String pattern) {
         if(pattern == null && str == null){
             return true;
@@ -24,22 +31,36 @@ public class RegularExpressionMatching {
             char charAtPattern = pattern.charAt(indexPattern);
             char charAtPatternNext = charAtNext(indexPattern,pattern);
 
-            if(charAtPattern == '.'){
-                if(charAtPatternNext == '*'){
+            if(charAtPattern == '.' && charAtPatternNext == '*'){
                     // 后续不用在看，肯定匹配
                     return true;
-                }
+            }else if(charAtPattern == '.' && charAtPatternNext != '*'){
                 // else 相当于匹配
                 indexStr ++;
                 indexPattern ++;
-            } else if(charAtPattern == '*'){
-                indexStr ++;
-                indexPattern ++;
+            } else if(charAtPattern != '.' && charAtPatternNext == '*'){
+                // 普通字符匹配
+                if(charAtPattern == charAtStr){
+                    indexStr ++;
+//                    indexPattern ++; // 不添加
+                } else {
+                    return false; // 不匹配
+                }
+            } else if(charAtPattern != '.' && charAtPatternNext != '*'){
+                // 普通字符匹配
+                if(charAtPattern == charAtStr){
+                    indexStr ++;
+                    indexPattern ++;
+                } else {
+                    return false; // 不匹配
+                }
             } else {
                 // 其他情况，比如普通字符，， ABCD
-                indexStr ++;
-                if(charAtPatternNext == '*'){
+                if(charAtPattern == charAtStr){
+                    indexStr ++;
                     indexPattern ++;
+                } else {
+                    return false; // 不匹配
                 }
             }
 
@@ -48,7 +69,7 @@ public class RegularExpressionMatching {
             }
         }
 
-        return false;
+        return true;
     }
 
     private char charAtNext(int indexPattern, String pattern) {
