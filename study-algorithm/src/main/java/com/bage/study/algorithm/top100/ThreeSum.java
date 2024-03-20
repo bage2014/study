9886e4f17964381c7fe5e83742d6964a7490ff02
bage <893542907@qq.com>
2024-03-21 00:07:49 +0800

    three sum

---
+++ study-algorithm/src/main/java/com/bage/study/algorithm/top100/ThreeSum.java
@@ -0,0 +1,14 @@
+package com.bage.study.algorithm.top100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.cn/problems/3sum/description/
+ */
+public class ThreeSum {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        return new ArrayList<>();
+    }
+}
