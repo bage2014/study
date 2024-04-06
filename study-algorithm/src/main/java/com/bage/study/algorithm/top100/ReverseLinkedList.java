b316371b4037178dd4cdc2289e98388ee46bd5d5
bage <893542907@qq.com>
2024-04-06 22:15:44 +0800

    revert linked list

---
+++ study-algorithm/src/main/java/com/bage/study/algorithm/top100/ReverseLinkedList.java
@@ -0,0 +1,13 @@
+package com.bage.study.algorithm.top100;
+
+import com.bage.study.algorithm.common.ListNode;
+
+/**
+ * https://leetcode.cn/problems/reverse-linked-list/description/
+ */
+public class ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+        return null;
+    }
+}
