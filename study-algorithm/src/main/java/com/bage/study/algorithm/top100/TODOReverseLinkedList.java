1c96b3989ed75393c8e6e6a9f1a9eaf9d909d5bc
bage <893542907@qq.com>
2024-04-11 08:44:57 +0800

    reverse linked list

--- study-algorithm/src/main/java/com/bage/study/algorithm/top100/TODOReverseLinkedList.java
+++ study-algorithm/src/main/java/com/bage/study/algorithm/top100/TODOReverseLinkedList.java
@@ -9,14 +9,49 @@ import com.bage.study.algorithm.common.ListNode;
 public class TODOReverseLinkedList {
 
     public static void main(String[] args) {
-        ListNode node = DataUtils.init(1,2,3,4,5);
-        ListNode result = new TODOReverseLinkedList().reverseList(node);
-        DataUtils.print(result);
+        ListNode node = DataUtils.init(1, 2, 3, 4, 5);
+        new TODOReverseLinkedList().reverseListR(node);
+        DataUtils.print(node);
     }
-    public ListNode reverseList(ListNode head) {
-        if(head == null){
-            return null;
+
+    public void reverseList(ListNode head) {
+        if (head == null) {
+            return;
         }
-        return null;
+
+        ListNode result = head;
+        if (head.next == null) {
+            return;
+        }
+
+        while (head.next.next != null) {
+
+        }
+        ListNode next2 = head.next.next;
+
+        // 交换 head 和 head.next
+        ListNode temp = head;
+        head = head.next;
+        head.next = temp;
+        temp.next = next2; // 原来的链
+
+        reverseList(temp.next);
+    }
+
+
+    public ListNode reverseListR(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        if (head.next == null) {
+            return head;
+        }
+
+        ListNode temp = head.next.next;
+
+        // 交换 head 和 head.next
+        head.next.next = head;
+        head.next = temp; // 原来的链
+        return reverseListR(head.next);
     }
 }
