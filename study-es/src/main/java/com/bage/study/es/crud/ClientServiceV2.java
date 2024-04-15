85256cc11ea4b8defbd94ce0d143aa274aa9479e
bage <893542907@qq.com>
2024-04-15 12:39:55 +0800

    es official demo

---
+++ study-es/src/main/java/com/bage/study/es/crud/ClientServiceV2.java
@@ -0,0 +1,39 @@
+package com.bage.study.es.crud;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.IndexResponse;
+import com.bage.study.es.model.Product;
+
+import java.io.IOException;
+
+public class ClientServiceV2 {
+
+    private ElasticsearchClient esClient = EsUtils.getClient();
+
+    public int createIndex(){
+        try {
+            esClient.indices().create(c -> c
+                    .index("products")
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return 0;
+    }
+    public void index(){
+        Product product = new Product("bk-1", "City bike", 123.0);
+
+        IndexResponse response = null;
+        try {
+            response = esClient.index(i -> i
+                    .index("products")
+                    .id(product.getSku())
+                    .document(product)
+            );
+            System.out.println("Indexed with version " + response.version());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
