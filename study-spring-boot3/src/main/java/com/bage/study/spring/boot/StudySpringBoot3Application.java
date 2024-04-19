928cb961a0a89dbcb42ea6e9fedfefa4b7ead77e
bage <893542907@qq.com>
2024-04-19 13:10:33 +0800

    spring boot 3

---
+++ study-spring-boot3/src/main/java/com/bage/study/spring/boot/StudySpringBoot3Application.java
@@ -0,0 +1,13 @@
+package com.bage.study.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringBoot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringBoot3Application.class, args);
+	}
+
+}
