a74935afac39d4e21ea766e925c92fd1efbce889
bage <893542907@qq.com>
2024-04-19 13:10:33 +0800

    spring boot 3

---
+++ study-spring-boot3/src/main/java/com/bage/study/spring/boot/HelloController.java
@@ -0,0 +1,13 @@
+package com.bage.study.spring.boot;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
+}
---
+++ study-spring-boot3/src/test/java/com/bage/study/spring/boot/StudySpringBoot3ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bage.study.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringBoot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
