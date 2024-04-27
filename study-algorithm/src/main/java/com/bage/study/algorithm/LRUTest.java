810f7b88b6401ab28c5e5e640e6a3d53ff8f49bd
bage <893542907@qq.com>
2024-04-27 19:51:59 +0800

    lru

---
+++ study-algorithm/src/main/java/com/bage/study/algorithm/LRUTest.java
@@ -0,0 +1,17 @@
+package com.bage.study.algorithm;
+
+/**
+ *
+ * LRU的全称为Least Recently Used，最近最少使用。
+ * 它是一种内存淘汰算法，当内存不够时，将内存中最久没使用的数据清理掉。
+ * LRU算法常用于缓存的淘汰策略。
+ *
+ */
+public class LRUTest {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
