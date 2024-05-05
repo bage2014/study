3b1c70525334763a6e42a480eadace9ef3e15c97
bage <893542907@qq.com>
2024-05-05 21:20:45 +0800

    CombinationSum

--- study-algorithm/src/main/java/com/bage/study/algorithm/top100/CombinationSum.java
+++ study-algorithm/src/main/java/com/bage/study/algorithm/top100/CombinationSum.java
@@ -1,13 +1,22 @@
 package com.bage.study.algorithm.top100;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * https://leetcode.cn/problems/combination-sum/
  */
 public class CombinationSum {
+
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+        int length = candidates.length;
+        for (int i = 0; i < length; i++) {
+            if(){
+
+            }
+        }
+        return result;
     }
 
 }
