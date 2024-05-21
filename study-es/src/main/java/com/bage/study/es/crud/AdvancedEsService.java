c79510f9d1e26a89bee9bf02302236e95445e262
bage <893542907@qq.com>
2024-05-22 07:51:41 +0800

    es java client

--- study-es/src/main/java/com/bage/study/es/crud/AdvancedEsService.java
+++ study-es/src/main/java/com/bage/study/es/crud/AdvancedEsService.java
@@ -1,15 +1,23 @@
 package com.bage.study.es.crud;
 
-import co.elastic.clients.elasticsearch.core.IndexRequest;
-import co.elastic.clients.elasticsearch.core.IndexResponse;
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+import co.elastic.clients.elasticsearch._types.query_dsl.QueryBuilders;
+import co.elastic.clients.elasticsearch.core.*;
+import co.elastic.clients.elasticsearch.core.bulk.BulkResponseItem;
+import co.elastic.clients.elasticsearch.core.search.Hit;
 import co.elastic.clients.json.JsonData;
+import com.bage.study.es.model.Person;
+import com.bage.study.es.model.Product;
 
+import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class AdvancedEsService {
-
-    private String index = "persons";
 
     public String withJson(String json) {
         try {
@@ -29,6 +37,69 @@ public class AdvancedEsService {
             e.printStackTrace();
         }
         return "";
+    }
+
+
+    public void bulkMultiDocuments() {
+        try {
+
+            List<Product> products = fetchProducts();
+
+            BulkRequest.Builder br = new BulkRequest.Builder();
+
+            for (Product product : products) {
+                br.operations(op -> op
+                        .index(idx -> idx
+                                .index("products")
+                                .id(product.getSku())
+                                .document(product)
+                        )
+                );
+            }
+
+            BulkResponse result = EsUtils.getClient().bulk(br.build());
+
+            // Log errors, if any
+            if (result.errors()) {
+                System.out.println("Bulk had errors");
+                for (BulkResponseItem item : result.items()) {
+                    if (item.error() != null) {
+                        System.out.println(item.error().reason());
+                    }
+                }
+            } else {
+                System.out.println("---- OK -----");
+                try {
+                    ElasticsearchClient client = EsUtils.getClient();
+                    Query query = QueryBuilders.ids().values("bk-1","bk-2","bk-3")
+                            .build()._toQuery();
+                    SearchResponse<Product> search = client.search(s -> s
+                                    .index("products")
+                                    .query(query)
+                            , Product.class);
+                    List<Hit<Product>> hits = search.hits().hits();
+
+                    for (Hit<Product> hit : hits) {
+                        System.out.println(hit.source());
+                    }
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private List<Product> fetchProducts() {
+        return Arrays.asList(
+                new Product("bk-1", "City bike", 123.0),
+                new Product("bk-2", "City bike2", 1234.0),
+                new Product("bk-3", "City bike3", 1253.0)
+        );
+
     }
 
 }
--- study-es/src/test/java/com/bage/study/es/EsTermsTest.java
+++ study-es/src/test/java/com/bage/study/es/EsTermsTest.java
@@ -1,6 +1,7 @@
 package com.bage.study.es;
 
 import com.bage.study.es.terms.EsTermService;
+import org.junit.Test;
 
 import java.io.IOException;
 
@@ -8,7 +9,8 @@ public class EsTermsTest {
 
     private static EsTermService esService = new EsTermService();
 
-    public static void main(String[] args) throws IOException {
+    @Test
+    public void main() throws IOException {
         esService.analyze("persons","hello world, bage ,hhhhh");
 
     }
