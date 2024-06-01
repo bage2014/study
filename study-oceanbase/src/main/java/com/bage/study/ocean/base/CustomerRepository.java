255cd2b2f4ee08a9552cfab17fd24decf4b5449d
bage <893542907@qq.com>
2024-06-01 19:33:27 +0800

    spring boot ocean base

---
+++ study-oceanbase/src/main/java/com/bage/study/ocean/base/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.bage.study.ocean.base;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+  List<Customer> findByLastName(String lastName);
+
+  Customer findById(long id);
+}
