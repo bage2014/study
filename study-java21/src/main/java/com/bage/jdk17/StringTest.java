fe9fcad186ee268a96a9b9c61d486f3620c02197
bage <893542907@qq.com>
2024-06-07 12:42:29 +0800

    jdk17

---
+++ study-java21/src/main/java/com/bage/jdk17/StringTest.java
@@ -0,0 +1,18 @@
+package com.bage.jdk17;
+
+public class StringTest {
+
+    public static void main(String[] args) {
+        System.out.println(getHtml());
+    }
+
+    private static String getHtml() {
+//        return "<div>hello world</div>";
+
+        return """
+                <div>
+                </div>
+                """;
+    }
+
+}
