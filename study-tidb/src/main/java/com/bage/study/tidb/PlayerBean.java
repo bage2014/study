72db8458b53d90b12e27481ba96aca564bf32ec0
bage <893542907@qq.com>
2024-06-26 08:58:10 +0800

    init

---
+++ study-tidb/src/main/java/com/bage/study/tidb/PlayerBean.java
@@ -0,0 +1,69 @@
+// Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.bage.study.tidb;
+
+import jakarta.persistence.*;
+
+/**
+ * it's core entity in hibernate
+ * @Table appoint to table name
+ */
+@Entity
+@Table(name = "player_jpa")
+public class PlayerBean {
+    /**
+     * @ID primary key
+     * @GeneratedValue generated way. this field will use generator named "player_id"
+     * @SequenceGenerator using `sequence` feature to create a generator,
+     *    and it named "player_jpa_id_seq" in database, initial form 1 (by `initialValue`
+     *    parameter default), and every operator will increase 1 (by `allocationSize`)
+     */
+    @Id
+    @GeneratedValue(generator="player_id")
+    @SequenceGenerator(name="player_id", sequenceName="player_jpa_id_seq", allocationSize=1)
+    private Long id;
+
+    /**
+     * @Column field
+     */
+    @Column(name = "coins")
+    private Integer coins;
+    @Column(name = "goods")
+    private Integer goods;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getCoins() {
+        return coins;
+    }
+
+    public void setCoins(Integer coins) {
+        this.coins = coins;
+    }
+
+    public Integer getGoods() {
+        return goods;
+    }
+
+    public void setGoods(Integer goods) {
+        this.goods = goods;
+    }
+}
