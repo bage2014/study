a60690920f9b5845ada659d7769d9817d19c6d01
bage <893542907@qq.com>
2024-07-01 08:40:39 +0800

    zookeeper init

--- study-zookeeper/src/main/java/com/bage/study/zookeeper/ZKManager.java
+++ study-zookeeper/src/main/java/com/bage/study/zookeeper/ZKManager.java
@@ -5,9 +5,7 @@ import org.apache.zookeeper.KeeperException;
 import java.io.UnsupportedEncodingException;
 
 public interface ZKManager {
-    public void create(String path, byte[] data)
-      throws KeeperException, InterruptedException;
-    public Object getZNodeData(String path, boolean watchFlag) throws UnsupportedEncodingException;
-    public void update(String path, byte[] data) 
-      throws KeeperException, InterruptedException;
+    void create(String path, byte[] data) throws Exception;
+    Object getZNodeData(String path, boolean watchFlag) throws Exception;
+    void update(String path, byte[] data) throws Exception;
 }
