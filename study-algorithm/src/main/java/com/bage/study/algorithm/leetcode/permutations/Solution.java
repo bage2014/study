dccee69b1135078cde1d59710199dceb0f60ab72
bage <893542907@qq.com>
2024-08-27 09:09:41 +0800

    Update Solution.java

--- study-algorithm/src/main/java/com/bage/study/algorithm/leetcode/permutations/Solution.java
+++ study-algorithm/src/main/java/com/bage/study/algorithm/leetcode/permutations/Solution.java
@@ -10,6 +10,10 @@ import java.util.List;
  */
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+
+
+        }
         return null;
     }
 
