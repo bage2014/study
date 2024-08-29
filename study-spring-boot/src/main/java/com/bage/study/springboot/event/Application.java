13d66a8a602ca4462bee2e191c6228136d41ad0c
bage <893542907@qq.com>
2024-08-29 23:20:27 +0800

    customer thread pool

--- study-spring-boot/src/main/java/com/bage/study/springboot/event/Application.java
+++ study-spring-boot/src/main/java/com/bage/study/springboot/event/Application.java
@@ -6,15 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
- *
  * ref: https://docs.spring.io/spring-framework/docs/current/reference/html/core.html#context-functionality-events
- *
- * 
  */
 @SpringBootApplication
 @RestController
@@ -45,6 +45,23 @@ public class Application implements CommandLineRunner {
         SimpleApplicationEventMulticaster simpleApplicationEventMulticaster = new SimpleApplicationEventMulticaster();
         simpleApplicationEventMulticaster.setTaskExecutor(simpleAsyncTaskExecutor());
         return simpleApplicationEventMulticaster;
+    }
+
+    /**
+     * 初始化一个线程池，放入spring beanFactory
+     *
+     * @return
+     */
+    @Bean(name = "asyncExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(200);
+        executor.setKeepAliveSeconds(60);
+        executor.setThreadNamePrefix("my-asyncExecutor-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        return executor;
     }
 
 }
