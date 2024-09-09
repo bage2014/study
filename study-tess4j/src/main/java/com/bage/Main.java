dfbfb941812fe7d37561290895d920229fb96f5c
bage <893542907@qq.com>
2024-09-10 02:49:36 +0800

    tess4j

---
+++ study-tess4j/src/main/java/com/bage/Main.java
@@ -0,0 +1,31 @@
+package com.bage;
+
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+import java.io.File;
+
+/**
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        // System.setProperty("jna.library.path", "32".equals(System.getProperty("sun.arch.data.model")) ? "lib/win32-x86" : "lib/win32-x86-64");
+
+        File imageFile = new File("eurotext.tif");
+        ITesseract instance = new Tesseract();  // JNA Interface Mapping
+        // ITesseract instance = new Tesseract1(); // JNA Direct Mapping
+        // File tessDataFolder = LoadLibs.extractTessResources("tessdata"); // Maven build bundles English data
+        // instance.setDatapath(tessDataFolder.getPath());
+
+        try {
+            String result = instance.doOCR(imageFile);
+            System.out.println(result);
+        } catch (TesseractException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Hello world!");
+    }
+
+}
