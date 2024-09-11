58e1a73c80beb6a6d73dc638a5ce72771d572275
bage <893542907@qq.com>
2024-09-11 08:17:01 +0800

    tv query result

--- study-app-tutorials/lib/request/model/tv/tv_query_request_param.dart
+++ study-app-tutorials/lib/request/model/tv/tv_query_request_param.dart
@@ -5,19 +5,31 @@ class TvQueryRequestParam {
   bool? isOnlyFavorite;
   int? targetPage;
   int? pageSize;
-
+  CommonParam? commonParam;
 
   TvQueryRequestParam(
       {this.favoriteUserId,
         this.isOnlyFavorite,
         this.targetPage,
-        this.pageSize});
+        this.pageSize,
+        this.commonParam});
 
-  CommonParam common = CommonParam();
-
-  @override
-  String toString() {
-    return super.toString();
+  TvQueryRequestParam.fromJson(Map<String, dynamic> json) {
+    favoriteUserId = json['favoriteUserId'];
+    isOnlyFavorite = json['isOnlyFavorite'];
+    targetPage = json['targetPage'];
+    pageSize = json['pageSize'];
+    commonParam = json['commonParam'];
   }
 
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['favoriteUserId'] = this.favoriteUserId;
+    data['isOnlyFavorite'] = this.isOnlyFavorite;
+    data['targetPage'] = this.targetPage;
+    data['pageSize'] = this.pageSize;
+    data['commonParam'] = this.commonParam;
+    return data;
+  }
 }
+
