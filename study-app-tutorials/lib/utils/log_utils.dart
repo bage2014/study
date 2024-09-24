32e0dccf914ca5c2dc69db2669bf83d3d46a4207
bage <893542907@qq.com>
2024-09-24 09:06:42 +0800

    profile info request

---
+++ study-app-tutorials/lib/utils/log_utils.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+import 'package:open_file/open_file.dart';
+import 'package:tutorials/component/cache/setting_caches.dart';
+import 'package:tutorials/component/log/logs.dart';
+
+class LogUtils {
+
+  static void info(Object object) {
+    print(object);
+  }
+}
