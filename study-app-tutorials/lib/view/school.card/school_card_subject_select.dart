c42ce9dc80a606efe07f75b7f8cfde4a6e5382f9
bage <893542907@qq.com>
2024-10-11 00:01:33 +0800

    select filter

--- study-app-tutorials/lib/view/school.card/school_card_subject_select.dart
+++ study-app-tutorials/lib/view/school.card/school_card_subject_select.dart
@@ -1,54 +1,80 @@
-import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:tutorials/component/log/logs.dart';
 import 'package:tutorials/locale/translations.dart';
+import 'package:tutorials/request/model/common/page_query_request_param.dart';
+import 'package:tutorials/request/origin/subject_meta_data_query_result.dart';
+import 'package:tutorials/request/school_subject_select_request.dart';
 import 'package:tutorials/utils/app_utils.dart';
-
+import 'package:tutorials/request/origin/school_meta_data_query_result.dart' as SchoolResult;
 
 class SchoolCardSubjectSelect extends StatefulWidget {
   const SchoolCardSubjectSelect({Key? key}) : super(key: key);
 
-
   @override
-  State<SchoolCardSubjectSelect> createState() => _SchoolCardSubjectSelectState();
+  State<SchoolCardSubjectSelect> createState() =>
+      _SchoolCardSubjectSelectState();
 }
 
 class _SchoolCardSubjectSelectState extends State<SchoolCardSubjectSelect> {
-
   TextEditingController _searchQueryController = TextEditingController();
   bool _isSearching = false;
+  List<Data> list = [];
+  bool _isLoading = false;
+  SchoolResult.Data? arg;
+
+  @override
+  void initState() {
+    super.initState();
+    _onRefresh(null);
+  }
 
   @override
   Widget build(BuildContext context) {
+    //获取路由参数
+    var args = ModalRoute.of(context)?.settings?.arguments;
+    Logs.info('SchoolCardSubjectSelect Data1=${arg?.toJson()}');
+    if(args is SchoolResult.Data) {
+      arg = args;
+      Logs.info('SchoolCardSubjectSelect Data=${arg?.toJson()}');
+    }
+
     return Scaffold(
       appBar: AppBar(
         leading: const BackButton(),
-        title: _isSearching ? _buildSearchField() : Text(Translations.textOf(context, "school.card.select.subject.title")),
+        title: _isSearching
+            ? _buildSearchField()
+            : Text(Translations.textOf(
+                context, "school.card.select.subject.title")),
         actions: _buildActions(),
       ),
-      body: LayoutBuilder(builder: (context, constraints) {
-        return SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Column(
-                children: List.generate(10, (index) {
-                  return ListItem();
-                }),
-              )
-            ],
-          ),
-        );
-      }),
+      body: Container(
+        child: _isLoading
+            ? Center(
+                child: CircularProgressIndicator(
+                backgroundColor: Colors.grey[200],
+                valueColor: const AlwaysStoppedAnimation(Colors.blue),
+              ))
+            : LayoutBuilder(builder: (context, constraints) {
+                return SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        children: List.generate(list.length, (index) {
+                          return ListItem(elementAt: list.elementAt(index));
+                        }),
+                      )
+                    ],
+                  ),
+                );
+              }),
+      ),
     );
-
   }
 
-
   Widget _buildSearchField() {
-    return
-    TextField(
+    return TextField(
       controller: _searchQueryController,
       autofocus: true,
       decoration: InputDecoration(
@@ -60,6 +86,7 @@ class _SchoolCardSubjectSelectState extends State<SchoolCardSubjectSelect> {
       onChanged: (value) {
         // place submit function here
         Logs.info("onFieldSubmitted=$value");
+        _onRefresh(value);
       },
     );
   }
@@ -73,6 +100,7 @@ class _SchoolCardSubjectSelectState extends State<SchoolCardSubjectSelect> {
             setState(() {
               _searchQueryController.clear();
               _isSearching = false;
+              _onRefresh('');
             });
           },
         ),
@@ -90,28 +118,60 @@ class _SchoolCardSubjectSelectState extends State<SchoolCardSubjectSelect> {
       ),
     ];
   }
+
+  void _onRefresh(String? keyword) {
+    showLoading();
+    CommonPageQueryRequestParam param = CommonPageQueryRequestParam();
+    param.keyword = keyword;
+    SchoolSubjectSelectRequests.querySubject(param).then((result) {
+      Logs.info('_onRefresh responseBody=' + (result?.toString() ?? ""));
+      hideLoading();
+      setState(() {
+        if (result.code == 200) {
+          list.clear();
+          var dataList = result.data ?? [];
+          for (var element in dataList) {
+            list.add(element);
+          }
+        }
+      });
+    }).catchError((error) {
+      Logs.info(error.toString());
+      hideLoading();
+    });
+  }
+
+  hideLoading() {
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  showLoading() {
+    setState(() {
+      _isLoading = true;
+    });
+  }
 }
 
 class ListItem extends StatelessWidget {
-  const ListItem({Key? key,}) : super(key: key);
-
+  const ListItem({Key? key, this.elementAt}) : super(key: key);
+  final Data? elementAt;
 
   @override
   Widget build(BuildContext context) {
-    var random = Random().nextInt(100).toString();
     return GestureDetector(
       onTap: () {
-        AppUtils.pop(context,random);
-        Logs.info("random=$random");
+        AppUtils.pop(context, elementAt);
       },
-      child: ListTile(
-        leading: const CircleAvatar(
-          backgroundImage: AssetImage('assets/images/school-logo.png'),
-        ),
-        title: Text("专业${random}" ),
+      child: Column(
+        children: [
+          const SizedBox(height: 16),
+          ListTile(
+            title: Text(elementAt?.name ?? ''),
+          )
+        ],
       ),
     );
-
-
   }
 }
