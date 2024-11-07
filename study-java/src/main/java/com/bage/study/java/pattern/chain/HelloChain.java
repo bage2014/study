d448bba261f321d2b796b99a77c5398a87159b14
bage <893542907@qq.com>
2024-11-07 23:03:47 +0800

    chain

---
+++ study-java/src/main/java/com/bage/study/java/pattern/chain/HelloChain.java
@@ -0,0 +1,20 @@
+package com.bage.study.java.pattern.chain;
+
+/**
+ * https://juejin.cn/post/7402435366647758857
+ *
+ * https://juejin.cn/post/7326920430589722639?searchId=20241107225601ACF3D170EDFED2D862D1
+ *
+ * https://juejin.cn/post/7280791145684189240?searchId=20241107225601ACF3D170EDFED2D862D1
+ *
+ * https://juejin.cn/post/7268260939066064952?searchId=20241107225601ACF3D170EDFED2D862D1
+ *
+ * https://juejin.cn/post/7122384707697770510?searchId=20241107225601ACF3D170EDFED2D862D1
+ *
+ *
+ */
+public class HelloChain {
+    public static void main(String[] args) {
+
+    }
+}
