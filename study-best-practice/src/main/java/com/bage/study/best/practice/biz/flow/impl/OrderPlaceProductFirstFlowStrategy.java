56b26fc87d1ec0150ba5fd9ee17dd2cdb6ffce60
bage <893542907@qq.com>
2024-11-13 08:37:12 +0800

    rename

--- study-best-practice/src/main/java/com/bage/study/best/practice/biz/flow/impl/OrderPlaceProductFirstFlowStrategy.java
+++ study-best-practice/src/main/java/com/bage/study/best/practice/biz/flow/impl/OrderPlaceProductFirstFlowStrategy.java
@@ -2,6 +2,7 @@ package com.bage.study.best.practice.biz.flow.impl;
 
 import com.bage.study.best.practice.biz.flow.OrderFlowStrategy;
 import com.bage.study.best.practice.biz.steps.order.comfirm.OrderStepOrderConfirmHandler;
+import com.bage.study.best.practice.biz.steps.order.comfirm.OrderStepSupplierConfirmHandler;
 import com.bage.study.best.practice.biz.steps.order.place.OrderStepOrderAddHandler;
 import com.bage.study.best.practice.biz.steps.order.place.OrderStepProductAddHandler;
 import com.bage.study.best.practice.biz.steps.order.place.OrderStepSupplierPlaceHandler;
@@ -9,11 +10,9 @@ import com.bage.study.best.practice.biz.steps.order.place.OrderStepValidCheckHan
 
 public class OrderPlaceProductFirstFlowStrategy implements OrderFlowStrategy {
     public static void main(String[] args) {
-        OrderStepValidCheckHandler start = new OrderStepValidCheckHandler();
-        start.setNext(new OrderStepProductAddHandler())
+        OrderStepOrderConfirmHandler start = new OrderStepOrderConfirmHandler();
+        start.setNext(new OrderStepSupplierConfirmHandler())
                 .setNext(new OrderStepOrderConfirmHandler())
-                .setNext(new OrderStepSupplierPlaceHandler())
-                .setNext(new OrderStepOrderAddHandler())
         ;
         start.execute("mock");
     }
