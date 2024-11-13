a2f63c57cbdd8a766b34b85f7fdb09850c78eb2d
bage <893542907@qq.com>
2024-11-14 07:18:32 +0800

    match

--- study-best-practice/src/main/java/com/bage/study/best/practice/biz/flow/impl/OrderPlaceFlowStrategy.java
+++ study-best-practice/src/main/java/com/bage/study/best/practice/biz/flow/impl/OrderPlaceFlowStrategy.java
@@ -1,17 +1,16 @@
 package com.bage.study.best.practice.biz.flow.impl;
 
-import com.bage.study.best.practice.biz.flow.OrderFlowStrategy;
 import com.bage.study.best.practice.biz.steps.order.place.OrderStepOrderAddHandler;
 import com.bage.study.best.practice.biz.steps.order.place.OrderStepProductAddHandler;
 import com.bage.study.best.practice.biz.steps.order.place.OrderStepSupplierPlaceHandler;
 import com.bage.study.best.practice.biz.steps.order.place.OrderStepValidCheckHandler;
 
-public class OrderPlaceFlowStrategy implements OrderFlowStrategy {
+public class OrderPlaceFlowStrategy extends OrderPlaceBaseFlowStrategy {
     public static void main(String[] args) {
         OrderStepValidCheckHandler start = new OrderStepValidCheckHandler();
-        start.setNext(new OrderStepOrderAddHandler())
-                .setNext(new OrderStepSupplierPlaceHandler())
-                .setNext(new OrderStepProductAddHandler())
+        start.next(new OrderStepOrderAddHandler())
+                .next(new OrderStepSupplierPlaceHandler())
+                .next(new OrderStepProductAddHandler())
         ;
         start.execute("mock");
     }
--- study-best-practice/src/main/java/com/bage/study/best/practice/biz/steps/order/place/OrderStepProductAddHandler.java
+++ study-best-practice/src/main/java/com/bage/study/best/practice/biz/steps/order/place/OrderStepProductAddHandler.java
@@ -1,16 +1,33 @@
 package com.bage.study.best.practice.biz.steps.order.place;
 
+import com.bage.study.best.practice.biz.domain.ProductDomainService;
 import com.bage.study.best.practice.biz.steps.AbstractOrderStepHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Random;
 
 public class OrderStepProductAddHandler extends AbstractOrderStepHandler {
+
+    @Autowired
+    private ProductDomainService productDomainService;
 
     @Override
     protected Boolean process(Object context) {
         // 订单流程处理
         boolean result = "mock".equals(context) || (new Random().nextInt(100)) > 100;
         System.out.println(this.getClass().getSimpleName() + "-executed-" + result);
+        if (result) {
+            productDomainService.add(context);
+        }
         return result;
+    }
+
+    @Override
+    public Boolean match(Object context) {
+        return super.match(context) && hasProduct(context);
+    }
+
+    private boolean hasProduct(Object context) {
+        return false;
     }
 }
