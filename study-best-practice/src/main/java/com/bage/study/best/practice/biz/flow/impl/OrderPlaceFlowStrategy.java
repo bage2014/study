f315496644e5fcc5057c4400fbed09d45f0a3dd8
bage <893542907@qq.com>
2024-11-13 08:37:12 +0800

    rename

--- study-best-practice/src/main/java/com/bage/study/best/practice/biz/flow/impl/OrderPlaceFlowStrategy.java
+++ study-best-practice/src/main/java/com/bage/study/best/practice/biz/flow/impl/OrderPlaceFlowStrategy.java
@@ -1,7 +1,6 @@
 package com.bage.study.best.practice.biz.flow.impl;
 
 import com.bage.study.best.practice.biz.flow.OrderFlowStrategy;
-import com.bage.study.best.practice.biz.steps.order.comfirm.OrderStepOrderConfirmHandler;
 import com.bage.study.best.practice.biz.steps.order.place.OrderStepOrderAddHandler;
 import com.bage.study.best.practice.biz.steps.order.place.OrderStepProductAddHandler;
 import com.bage.study.best.practice.biz.steps.order.place.OrderStepSupplierPlaceHandler;
@@ -11,7 +10,6 @@ public class OrderPlaceFlowStrategy implements OrderFlowStrategy {
     public static void main(String[] args) {
         OrderStepValidCheckHandler start = new OrderStepValidCheckHandler();
         start.setNext(new OrderStepOrderAddHandler())
-                .setNext(new OrderStepOrderConfirmHandler())
                 .setNext(new OrderStepSupplierPlaceHandler())
                 .setNext(new OrderStepProductAddHandler())
         ;
