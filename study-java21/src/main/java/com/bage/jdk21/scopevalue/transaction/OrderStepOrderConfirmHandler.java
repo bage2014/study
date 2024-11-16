57ff221792f7cb50dff3f7e732c6fbb543c7444f
bage <893542907@qq.com>
2024-11-16 22:26:51 +0800

    rename

--- study-java21/src/main/java/com/bage/jdk21/scopevalue/transaction/OrderStepOrderConfirmHandler.java
+++ study-java21/src/main/java/com/bage/jdk21/scopevalue/transaction/OrderStepOrderConfirmHandler.java
@@ -1,7 +1,5 @@
 package com.bage.jdk21.scopevalue.transaction;
 
-import java.util.Random;
-
 public class OrderStepOrderConfirmHandler extends StepHandler<BaseContext> {
 
     OrderConfirmDomain1Service domain1Service = new OrderConfirmDomain1Service();
@@ -9,12 +7,9 @@ public class OrderStepOrderConfirmHandler extends StepHandler<BaseContext> {
     protected Boolean process(BaseContext context) {
         // 订单流程处理
         domain1Service.process();
-        return true;
-    }
 
-    @Override
-    public Integer transaction(BaseContext context) {
-        return domain1Service.transaction();
+        addTransactionStep(context,()->domain1Service.process());
+        return true;
     }
 
 }
