c93ace8a9127b677d4bb298380efe2cfcade2885
bage <893542907@qq.com>
2024-11-23 22:46:06 +0800

    collections

--- study-java21/src/main/java/com/bage/jdk21/collections/SequencedCollectionTest.java
+++ study-java21/src/main/java/com/bage/jdk21/collections/SequencedCollectionTest.java
@@ -2,9 +2,12 @@ package com.bage.jdk21.collections;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class SequencedCollectionTest {
     public static void main(String[] args) {
+        // addFirst addLast
         List<Integer> list = new ArrayList<>();
         list.addLast(1);
         list.addFirst(2);
@@ -12,6 +15,21 @@ public class SequencedCollectionTest {
         list.addFirst(4);
         list.addFirst(7);
         System.out.println(list);
+
+        // of
+        List.of();
+        List.of("Hello", "World");
+        List.of(1, 2, 3);
+        Set.of();
+        Set.of("Hello", "World");
+        Set.of(1, 2, 3);
+        Map.of();
+        Map.of("Hello", 1, "World", 2);
+
+        // takeWhile
+        List<Integer> integerList = List.of(11, 33, 66, 8, 9, 13);
+        integerList.stream().takeWhile(x -> x < 50).forEach(System.out::println);// 11 33
+
 
     }
 }
