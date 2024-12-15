3223996bef4c6b3472391c10c9542f25f0524f83
bage <893542907@qq.com>
2024-12-15 23:05:24 +0800

    spring boot common log

---
+++ study-spring-boot3/README.md
@@ -0,0 +1,1 @@
+https://juejin.cn/post/7417630844100231206
---
+++ study-spring-boot3/src/main/java/com/bage/study/spring/boot/RequestLoggingConfig.java
@@ -0,0 +1,19 @@
+package com.bage.study.spring.boot;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingConfig {
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setIncludeHeaders(true);
+        filter.setIncludeClientInfo(true);
+        filter.setAfterMessagePrefix("REQUEST DATA-");
+        return filter;
+    }
+}
