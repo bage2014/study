0a0732e54d6b1c7bc23e8e3ca1dbb71e98b19e11
bage <893542907@qq.com>
2024-12-16 20:24:07 +0800

    http request wrapper

--- study-spring-boot3/src/main/java/com/bage/study/spring/boot/HelloController.java
+++ study-spring-boot3/src/main/java/com/bage/study/spring/boot/HelloController.java
@@ -1,13 +1,24 @@
 package com.bage.study.spring.boot;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
+        return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/wrapper")
+    public String wrapper(HttpServletRequest request, HttpServletResponse response) {
+        return String.format("Hello %s!", request.getAttribute("X-Signature-request")) +
+                String.format("Hello %s!", response.getHeaders("X-Signature"))
+                ;
     }
 }
