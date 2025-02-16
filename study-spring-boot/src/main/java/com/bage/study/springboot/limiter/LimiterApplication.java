d11bbc0edfbd515dc966777ab5e2573851e130fa
bage <893542907@qq.com>
2025-02-16 22:03:14 +0800

    spring boot stream limiter

---
+++ study-spring-boot/src/main/java/com/bage/study/springboot/limiter/LimiterApplication.java
@@ -0,0 +1,14 @@
+package com.bage.study.springboot.limiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LimiterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimiterApplication.class, args);
+    }
+
+}
