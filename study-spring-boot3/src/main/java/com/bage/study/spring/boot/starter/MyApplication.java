d43ed50f3e3162854402d5b6e7ea1b826164241a
bage <893542907@qq.com>
2025-03-25 22:19:29 +0800

    canal

---
+++ study-spring-boot3/src/main/java/com/bage/study/spring/boot/starter/MyApplication.java
@@ -0,0 +1,16 @@
+package com.bage.study.spring.boot.starter;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ * 链接：https://juejin.cn/post/7479452347714945087
+ */
+public class MyApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(MyApplication.class)
+               .profiles("dev")
+               .properties("server.port=8081")
+                // .other config 
+               .run(args);
+    }
+}
