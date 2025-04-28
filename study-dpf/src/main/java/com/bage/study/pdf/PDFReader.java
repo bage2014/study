7af4b9a1e8d3bbbf086d3c8eee90862cfea3cf8a
bage <893542907@qq.com>
2025-04-28 20:54:18 +0800

    json compare

--- study-dpf/src/main/java/com/bage/study/pdf/PDFReader.java
+++ study-dpf/src/main/java/com/bage/study/pdf/PDFReader.java
@@ -3,6 +3,10 @@ package com.bage.study.pdf;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+import com.itextpdf.kernel.pdf.canvas.parser.listener.ITextExtractionStrategy;
+import com.itextpdf.kernel.pdf.canvas.parser.listener.SimpleTextExtractionStrategy;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
 
 
 import java.io.File;
@@ -11,9 +15,18 @@ import java.io.IOException;
 public class PDFReader {
     public static void main(String[] args) {
 
+        try (PDDocument document = Loader.loadPDF(new RandomAccessReadBufferedFile("yourfile.pdf")))
+        {
+            for (PDPage page : document.getPages())
+            {
+
+            }
+        }
+
         try {
             PdfDocument pdfDoc = new PdfDocument(new PdfReader("/Users/bage/Downloads/STR_Form.pdf"));
-            String text = PdfTextExtractor.getTextFromPage(pdfDoc, 1); // 提取第一页的文本
+            ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+            String text = PdfTextExtractor.getTextFromPage(pdfDoc, strategy); // 提取第一页的文本
             System.out.println(text);
             pdfDoc.close();
         } catch (IOException e) {
