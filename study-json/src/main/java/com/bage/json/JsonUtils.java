883ce3df3da0875ed177706064b86d6b24a4bf1f
bage <893542907@qq.com>
2025-05-07 20:44:15 +0800

    compare testcase

--- study-json/src/main/java/com/bage/json/JsonUtils.java
+++ study-json/src/main/java/com/bage/json/JsonUtils.java
@@ -2,6 +2,8 @@ package com.bage.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.lang.reflect.Type;
 
@@ -18,5 +20,9 @@ public class JsonUtils {
 
     public static <T> T fromJson(String json, Type typeOfT) {
         return gson.fromJson(json,typeOfT);
+    }
+
+    public static JsonObject parse(String json) {
+        return new JsonParser().parse(json).getAsJsonObject();
     }
 }
