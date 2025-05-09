e76a9fa3f65424506633bb0c536d37f6d95ac2cd
bage <893542907@qq.com>
2025-05-09 09:55:05 +0800

    ref

--- study-json/src/main/java/com/bage/json/compare/JsonCompareHelper.java
+++ study-json/src/main/java/com/bage/json/compare/JsonCompareHelper.java
@@ -14,6 +14,9 @@ import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+/**
+ * https://javers.org/documentation/diff-examples/
+ */
 public class JsonCompareHelper {
 
     ConfigProp configProp = new ConfigProp();
