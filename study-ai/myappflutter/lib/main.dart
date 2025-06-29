c8596a099c5b259172e7816bcbd01ebcca7b0f58
bage <893542907@qq.com>
2025-06-29 16:57:35 +0800

    map

--- study-ai/myappflutter/lib/main.dart
+++ study-ai/myappflutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myappflutter/pages/map_trajectory.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MapTrajectory(),
     );
   }
 }
