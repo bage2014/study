d4e3336b2b89f803aaa6d8cc908bad13b4fb0a01
bage <893542907@qq.com>
2025-06-29 15:01:18 +0800

    end point fix

--- study-ai/endpoint/src/main/java/com/example/demo/repository/ActivityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Activity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ActivityRepository extends JpaRepository<Activity, Long> {
-}
--- study-ai/endpoint/src/main/java/com/example/demo/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
