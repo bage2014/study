6fa55f672f5be151d6234747b6e3c1caf0b5292a
bage <893542907@qq.com>
2025-07-04 08:26:47 +0800

    flutter baidu location

--- study-ai/myappflutter/android/app/build.gradle.kts
+++ study-ai/myappflutter/android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         // minSdk = flutter.minSdkVersion
-        minSdk = 22
+        minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
