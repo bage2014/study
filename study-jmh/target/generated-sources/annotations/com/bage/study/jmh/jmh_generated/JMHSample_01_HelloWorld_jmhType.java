f6e71c5a05906a605e0d5af33abde4493c8b576d
bage <893542907@qq.com>
2026-06-24 23:22:08 +0800

    删除无用文件

--- study-jmh/target/generated-sources/annotations/com/bage/study/jmh/jmh_generated/JMHSample_01_HelloWorld_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.bage.study.jmh.jmh_generated;
-public class JMHSample_01_HelloWorld_jmhType extends JMHSample_01_HelloWorld_jmhType_B3 {
-}
-
--- study-jmh/target/generated-sources/annotations/com/bage/study/jmh/jmh_generated/JMHSample_02_BenchmarkModes_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.bage.study.jmh.jmh_generated;
-public class JMHSample_02_BenchmarkModes_jmhType extends JMHSample_02_BenchmarkModes_jmhType_B3 {
-}
-
