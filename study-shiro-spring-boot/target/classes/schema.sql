1f81056b72ae54f4c24634868af5ddf003f07e05
bage2014 <893542907@qq.com>
2026-06-24 23:13:27 +0800

    Delete study-shiro-spring-boot/target directory

--- study-shiro-spring-boot/target/classes/schema.sql
+++
@@ -1,8 +0,0 @@
---------------- H2 ---------------
-drop table if exists auth_path_definition;
-
-create table if not exists auth_path_definition (
-  id int PRIMARY KEY,
-  ant_path VARCHAR(255),
-  definition VARCHAR(255)
-);
